a85347c40618bbdda93ce1fa2245f5ce748942e8
iMissile <ishutov@gmail.com>
2015-05-10 11:35:23 +0300

    Linear regression
    Перевел на dplyr и добавил линейную регрессию по деньгам для подсчета
    даты выхода на 0

--- 40 Opaliha_consumption/consumption_analysis.R
+++ 40 Opaliha_consumption/consumption_analysis.R
@@ -1,20 +1,39 @@
 library(lubridate)
 library(ggplot2)
+library(magrittr)
+library(dplyr)
+library(scales)
+library(forecast)
+
+
 
 # http://stackoverflow.com/questions/5796924/how-can-i-determine-the-current-directory-name-in-r
-getwd()
-setwd("R:/Ilya/Опалиха/потребление_ресурсов")  # note / instead of \ in windows 
-mydata <- read.table("gas_consumption.csv", header=TRUE, stringsAsFactors = FALSE,
-                     sep=";")
+#getwd()
+#setwd("R:/Ilya/Опалиха/потребление_ресурсов")  # note / instead of \ in windows 
+filename <- "gas_consumption.csv"
+filename <- "electricity_consumption.csv"
 
-str(mydata)
-mydata$date <- dmy(mydata$date)
-str(mydata)
+mydata <- read.table(filename, header=TRUE, stringsAsFactors = FALSE, sep=";")
 
-# сделаем выборку по датам
+#str(mydata)
+mydata$date <- dmy_hm(mydata$date, truncated=2, tz="Europe/Moscow") #truncated -- могут быть опущены 2 параметра (часы, минуты)
+dplyr::rename(mydata, timestamp=date)
+#str(mydata)
+
+
+
+# =================== окно наблюдения ======================
+s_date <- dmy_hms("01.01.2015 0:0:0", tz="Europe/Moscow")
+e_date <- s_date + month(2) - days(1) # months(0.5)
+e_date = Sys.time()
+
+
+subdata <- rename(mydata, timestamp=date) %>%
+           filter(timestamp > s_date & timestamp < e_date)
+
 # http://stackoverflow.com/questions/17244077/select-subset-by-date-in-r
 # subdata <- mydata[ymd(20121201) < mydata$date & mydata$date < ymd(20130101),]
-subdata <- mydata[ymd(20120101) < mydata$date,]
+#subdata <- mydata[ymd(20120101) < mydata$date,]
 
 # альтернативный вариант -- subset
 # http://stackoverflow.com/questions/8992585/creating-a-data-frame-based-on-day-of-week-from-a-data-frame-containing-several
@@ -22,28 +41,50 @@ subdata <- mydata[ymd(20120101) < mydata$date,]
 # geom_point docs: http://docs.ggplot2.org/0.9.3.1/geom_point.html
 # http://sape.inf.usi.ch/quick-reference/ggplot2/geom_point
 
-p <- ggplot(subdata, aes(x=date, y=balance_m3))
-p <- ggplot(subdata, aes(x=date, y=balance_rub), size=I(5))
-p <- p + 
+#gp <- ggplot(subdata, aes(x=timestamp, y=balance_m3))
+#gp <- ggplot(subdata, aes(x=timestamp, y=balance_kwth))
+gp <- ggplot(subdata, aes(x=timestamp, y=balance_rub))
+gp <- gp +
   geom_point(size=4, shape=21) + # produce scatterplot # fill="green",
-  xlab("Дата") +
-  ylab("Баланс, руб") +
   geom_line() + 
-  stat_smooth(aes(outfit=fit<<-..y..)) #fitting http://stackoverflow.com/questions/9789871/method-to-extract-stat-smooth-line-fit
+#  stat_smooth(aes(outfit=fit<<-..y..)) + #fitting http://stackoverflow.com/questions/9789871/method-to-extract-stat-smooth-line-fit
+  theme_bw() +
+  scale_x_datetime(labels = date_format("%d.%m (%a)"), minor_breaks = date_breaks("1 days")) +
+  xlab("Дата") +
+  ylab("Баланс, руб")
 
-p
-#======
-
-x <- c("2010-04-14-04-35-59", "2010-04-01-12-00-00")
-str(ymd_hms(x))
+gp
 
 
+# используем линейную регрессию для определения выхода баланса денег на ноль
+# http://www.tatvic.com/blog/linear-regression-using-r/
+fit <-lm(balance_rub ~ timestamp, data=subdata)
+summary(fit)
+# дата, когда все выйдет в ноль
+str(fit$coefficients)
+# http://stackoverflow.com/questions/7257263/converting-date-times-in-posixct-gives-screwy-result
+# the origin of time for the "POSIXct" class, ‘1970-01-01 00:00.00 UTC’, is before UTC was defined
+# http://stat.ethz.ch/R-manual/R-devel/library/base/html/as.POSIXlt.html
+zerotime <- as.POSIXct(as.numeric(-fit$coefficients[1]/fit$coefficients[2]), 
+           tz="Europe/Moscow", 
+           origin=as.POSIXct(strptime("1970-01-01 00:00:00", "%Y-%m-%d %H:%M:%S", tz="UTC")))
+dput(zerotime)
 
-setClass('myDate')
-setAs("character","myDate", function(from) ymd_hms(from) )
+# а теперь нарисуем график до даты выхода на ноль
 
-tmp <- c("1, 15/08/2008", "2, 23/05/2010")
-con <- textConnection(tmp)
+df <- data.frame(timestamp = zerotime, balance_rub=0, balance_kwth = NA)
+# http://stackoverflow.com/questions/7476022/geom-point-and-geom-line-for-multiple-datasets-on-same-graph-in-ggplot2
+# http://www.cookbook-r.com/Graphs/Shapes_and_line_types/
+gp2 <- ggplot(rbind(subdata, df), aes(x=timestamp, y=balance_rub)) +
+  geom_point(size=4, shape=21) + # produce scatterplot # fill="green",
+  geom_line(color = "blue", linetype="dashed") + 
+  geom_point(data=df, size=4, shape=21, color="red", fill="yellow") +  # predicted
+  #  stat_smooth(aes(outfit=fit<<-..y..)) + #fitting http://stackoverflow.com/questions/9789871/method-to-extract-stat-smooth-line-fit
+  theme_bw() +
+  scale_x_datetime(labels = date_format("%d.%m (%a)"), minor_breaks = date_breaks("1 days")) +
+  xlab("Дата") +
+  ylab("Баланс, руб")
 
-tmp2 <- read.csv(con, colClasses=c('numeric','myDate'), header=FALSE)
-str(tmp2)
+gp2
+
+
