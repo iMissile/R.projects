e5ef616086dd3c79af6be182481eadbf39db6704
iMissile <ishutov@gmail.com>
2016-04-04 12:29:39 +0300

    charge distribution
    рисует окружность, точки и векторное поле сил

--- 42 IoT_test/sensor_on_disk.R
+++ 42 IoT_test/sensor_on_disk.R
@@ -1,5 +1,11 @@
+rm(list=ls()) # очистим все переменные
+
 library(dplyr)
 library(magrittr)
+library(ggplot2) #load first! (Wickham)
+library(lubridate) #load second!
+library(scales)
+
 
 # tidyr 0.4.0 Nested data frames
 # http://blog.rstudio.org/2016/02/02/tidyr-0-4-0/
@@ -7,7 +13,7 @@ library(magrittr)
 # Генерим точки внутри окружности единичного радиуса.
 # Считаем, что все частицы единичного заряда, поэтому его опускаем
 
-df.particles = data.frame(x = c(1, 2, 3), y = c(7, 8, 9))
+df.particles = data.frame(x = c(0.1, 0.2, 0.3), y = c(0.3, 0.4, 0.5), fixed = c (0, 0, 0))
 # lapply(particles, function(x){print(paste0("-", x, "-"))})
 
 
@@ -33,14 +39,79 @@ for (i in 1:nrow(df)){
     # а можно это делать и раньше, исключая строки с нулевым расстоянием
   
   
-  df1 <- df.step %>%
-    summarise(force.x = sum(force.x0), force.y = sum(force.y0))
+  # df1 <- df.step %>%
+  #  summarise(force.x = sum(force.x0), force.y = sum(force.y0))
   
   # сумма векторов даст результирующее воздействие на частицу, которое и необходимо выдать наружу
+  df$force.x[i] <- sum(df.step$force.x0)
+  df$force.y[i] <- sum(df.step$force.y0)
   
   print(paste0("step #", i))
   print(paste0("(", x0, ", ", y0, ") <-> force (", df1$force.x, ", ", df1$force.y, ")"))
+  
 }
+
+# рисование окружности на ggplot
+# http://stackoverflow.com/questions/6862742/draw-a-circle-with-ggplot2
+circleFun <- function(center = c(0,0), diameter = 1, npoints = 100){
+  r = diameter / 2
+  tt <- seq(0,2*pi,length.out = npoints)
+  xx <- center[1] + r * cos(tt)
+  yy <- center[2] + r * sin(tt)
+  return(data.frame(x = xx, y = yy))
+}
+
+disk_plot <- function(df) {
+  # отрисуем расположение точек и действующие силы
+  # Cookbook по работе с графикой, общий заход: http://www.cookbook-r.com/Graphs/
+  # радиус = 1, координаты [-1; 1] по обеим осям
+  
+  circdat <- circleFun(c(0,0), 2, npoints = 100)
+
+  gp <- ggplot(data = df, aes(x = x, y = y)) +
+    theme_bw() +
+    theme(
+      # text = element_text(size = 20), #, family = "verdana"),
+      # axis.text.x = element_text(vjust = 3),
+      # axis.text.y = element_text(hjust=3),
+      # axis.title.x = element_text(angle = 0, hjust = .5, vjust = 0, face = "plain"),
+      # axis.title.y = element_text(angle = 90, hjust = .5, vjust = 2, face = "plain")
+    ) +
+    # управление формой и цветом
+    # http://www.cookbook-r.com/Graphs/Shapes_and_line_types/
+    # http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
+    # scale_colour_brewer(palette = "Dark2", name  = paste(exp.env, "\nangle = ", val, "\n", sep = "")) +
+    #geom_line(size = 1, color = RColorBrewer::brewer.pal(3, "Set2")[1]) +
+    geom_point(size = 2, fill = "black", shape = 21) +    # White fill
+    # рисуем векторное поле
+    # http://stackoverflow.com/questions/14936504/vector-field-visualisation-r
+    # http://blog.revolutionanalytics.com/2013/02/a-review-of-the-r-graphics-cookbook.html
+    geom_segment(aes(xend = x + force.x/600, yend = y + force.y/600), colour = "red", 
+                 arrow = arrow(length = unit(0.2,"cm")), size = 0.6) + 
+    
+    # рисуем окружность
+    # http://stackoverflow.com/questions/6862742/draw-a-circle-with-ggplot2
+    geom_path(data = circdat, aes(x, y)) + # geom_path will do open circles, geom_polygon will do filled circles
+  
+    # geom_line(size = 1.5, aes(color = type)) +
+    # http://ggplot.yhathq.com/docs/xlim.html
+    xlim(-1, 1) +
+    ylim(-1, 1) +
+    
+    # http://www.cookbook-r.com/Graphs/Facets_(ggplot2)/
+    # This is done by giving a formula to facet_grid(), of the form vertical ~ horizontal.
+    # насчет масштабирования осей см http://www.cookbook-r.com/Graphs/Facets_(ggplot2)/#free-scales
+    #facet_grid(. ~ type, scales = "free_y") +
+    labs(x = "Ось X", y = "Ось Y")
+  
+  gp
+}
+
+
+
+
+disk_plot(df)
+
 
 print("End...")
 stop()
