d36dd546a03d89316d3c60a47453b21110eb0e7b
iMissile <ishutov@gmail.com>
2016-04-28 12:08:25 +0300

    моделируем погодные данные

--- 46 PoC_dashboard/prepare_data.R
+++ 46 PoC_dashboard/prepare_data.R
@@ -62,79 +62,139 @@ generate_field_data <- function(ofile = "tsensors.csv", back_days = 7, forward_d
   )
 }
 
+generate_weather_data <- function(ofile = "tweather.csv", back_days = 7, forward_days = 7) {
+  # генерируем погодные даты
+  
+  tick.seq <- seq(now() - days(back_days), now() + days(forward_days), by = "4 hours") # http://stackoverflow.com/questions/10887923/hourly-date-sequence-in-r
 
-# =================== main ==================
+  # дл€ подготовки данных 
+  # собираем сразу data.frame: врем€, #сенсора, показание
+  n <- length(tick.seq)
+  mydata <- data.frame(timestamp = tick.seq,
+                       temp.min = rnorm(n, 12, 5), # используем методику дополнени€
+                       temp.max = rnorm(n, 25, 5),
+                       temp = rnorm(n, 17, 5),
+                       pressure = rnorm(n, 750, 30),
+                       humidity = runif(n, 10, 100),
+                       precipitation = runif(n, 0, 20))
+  write.table(
+    mydata,
+    file = ofile,
+    sep = ",",
+    row.names = FALSE,
+    qmethod = "double"
+  )
+}
 
-ofile <- ".\\data\\tsensors.csv"
-generate_field_data(ofile, back_days = 7, forward_days = 0)
+plot_field_data <- function(ifile = ".\\data\\test_data.csv") {
+  # отобразим за указанный диапазон график: среднее, стандартное отклонение в виде серой области, выбросы
 
-# отобразим за указанный диапазон график: среднее, стандартное отклонение в виде серой области, выбросы
-
-ifile <- ofile
-ifile <- ".\\data\\test_data.csv"
-# подгружаем данные по сенсорам
-raw.df <- read_delim(ifile, delim = ",", quote = "\"",
-                     col_names = TRUE,
-                     locale = locale("ru", encoding = "windows-1251", tz = "Europe/Moscow"), # таймзону, в принципе, можно установить здесь
-                     # col_types = list(date = col_datetime(format = "%d.%m.%Y %H:%M")), 
-                     progress = interactive()
-) # http://barryrowlingson.github.io/hadleyverse/#5
-
-# сформируем часовые группы и посчитаем среднее по ансамблю сенсоров за каждую часовую группу
-# raw.df["time.group"] <- lapply(raw.df$timestamp, function(x){round(x, units="hours")})
-# t <- raw.df$timestamp
-# object.size(t)
-
-# m <- lapply(raw.df$timestamp, function(x){round(x, units="hours")}) # так из 130 кб получаем 35 ћб, надо использовать round_date {lubridate}
-# m <- lapply(t, function(x){round_date(x, unit = "hour")}) # тут получаем 8ћб !!!
-# m <- round_date(t, unit = "hour") # самый быстрый и компактный вариант
-# object.size(m)
-# object.size(m[[2]])
-raw.df["timegroup"] <- round_date(raw.df$timestamp, unit = "hour")
-
-# t.df <- raw.df %>%
-#   filter(timegroup < lubridate::now()) %>%
-#   filter(timegroup > lubridate::now() - days(3))
+  # подгружаем данные по сенсорам
+  raw.df <- read_delim(ifile, delim = ",", quote = "\"",
+                       col_names = TRUE,
+                       locale = locale("ru", encoding = "windows-1251", tz = "Europe/Moscow"), # таймзону, в принципе, можно установить здесь
+                       # col_types = list(date = col_datetime(format = "%d.%m.%Y %H:%M")), 
+                       progress = interactive()
+  ) # http://barryrowlingson.github.io/hadleyverse/#5
+  
+  # сформируем часовые группы и посчитаем среднее по ансамблю сенсоров за каждую часовую группу
+  # raw.df["time.group"] <- lapply(raw.df$timestamp, function(x){round(x, units="hours")})
+  # t <- raw.df$timestamp
+  # object.size(t)
+  
+  # m <- lapply(raw.df$timestamp, function(x){round(x, units="hours")}) # так из 130 кб получаем 35 ћб, надо использовать round_date {lubridate}
+  # m <- lapply(t, function(x){round_date(x, unit = "hour")}) # тут получаем 8ћб !!!
+  # m <- round_date(t, unit = "hour") # самый быстрый и компактный вариант
+  # object.size(m)
+  # object.size(m[[2]])
+  raw.df["timegroup"] <- round_date(raw.df$timestamp, unit = "hour")
+  
+  # t.df <- raw.df %>%
+  #   filter(timegroup < lubridate::now()) %>%
+  #   filter(timegroup > lubridate::now() - days(3))
+  
+  avg.df <- raw.df %>%
+    group_by(location, timegroup) %>%
+    summarise(value.mean = mean(value), value.sd = sd(value))
+  
+  object.size(raw.df)
+  object.size(avg.df)  
   
   
-
-
-avg.df <- raw.df %>%
-  group_by(location, timegroup) %>%
-  summarise(value.mean = mean(value), value.sd = sd(value))
-
-object.size(raw.df)
-object.size(avg.df)  
-
-
-# ggplot(avg.df, aes(timegroup, value.mean)) +
-#   geom_point() +
-#   geom_smooth(method="loess", level = 0.99999)
-
-ggplot(avg.df, aes(timegroup, value.mean, colour = factor(location))) +
-  # ggtitle("√рафик температуры") +
-  geom_point() +
-  geom_line() +
-  ylim(0, NA) +
-  theme_solarized() +
-  scale_colour_solarized("blue") +
-  theme(panel.border = element_rect(colour = "black", fill=NA, size = 2))
+  # ggplot(avg.df, aes(timegroup, value.mean)) +
+  #   geom_point() +
+  #   geom_smooth(method="loess", level = 0.99999)
+  
+  ggplot(avg.df, aes(timegroup, value.mean, colour = factor(location))) +
+    # ggtitle("√рафик температуры") +
+    geom_point() +
+    geom_line() +
+    ylim(0, NA) +
+    theme_solarized() +
+    scale_colour_solarized("blue") +
+    theme(panel.border = element_rect(colour = "black", fill=NA, size = 2))
   # theme_solarized(light = TRUE) +
   # scale_colour_solarized("blue")
   # theme_hc() +
   # scale_colour_hc()
+  
+  
+  # http://docs.ggplot2.org/current/geom_boxplot.html
+  # You can also use boxplots with continuous x, as long as you supply a grouping variable.
+  # http://stackoverflow.com/questions/23433776/change-thickness-of-the-whole-line-geom-boxplot
+  ggplot(raw.df, aes(timestamp, value)) +
+    # geom_point(shape = 1) +
+    # geom_line() +
+    geom_jitter(width = 0.2) +
+    ylim(0, NA) +
+    geom_boxplot(aes(group = cut_width(timestamp, 86400/5))) + 
+    geom_smooth(method="loess", level = 0.99999)
+}
+
+plot_weather_data <- function(ifile = ".\\data\\tweather.csv") {
+  
+  raw.df <- read_delim(
+    ifile,
+    delim = ",",
+    quote = "\"",
+    col_names = TRUE,
+    locale = locale("ru", encoding = "windows-1251", tz = "Europe/Moscow"),
+    # таймзону, в принципе, можно установить здесь
+    # col_types = list(date = col_datetime(format = "%d.%m.%Y %H:%M")),
+    progress = interactive()
+  ) # http://barryrowlingson.github.io/hadleyverse/#5
+  
+  # сформируем часовые группы и посчитаем среднее по ансамблю сенсоров за каждую часовую группу
+  raw.df["timegroup"] <- round_date(raw.df$timestamp, unit = "hour")
+  
+
+  # https://www.datacamp.com/community/tutorials/make-histogram-ggplot2
+  ggplot(raw.df, aes(timestamp, temp)) +
+    # ggtitle("√рафик температуры") +
+    geom_point() +
+    geom_line() +
+    ylim(0, NA) +
+    theme_solarized() +
+    scale_colour_solarized("blue") +
+    theme(panel.border = element_rect(colour = "black", fill=NA, size = 2))
+  # theme_solarized(light = TRUE) +
+  # scale_colour_solarized("blue")
+  # theme_hc() +
+  # scale_colour_hc()
+}
 
 
-# http://docs.ggplot2.org/current/geom_boxplot.html
-# You can also use boxplots with continuous x, as long as you supply a grouping variable.
-# http://stackoverflow.com/questions/23433776/change-thickness-of-the-whole-line-geom-boxplot
-ggplot(raw.df, aes(timestamp, value)) +
-  # geom_point(shape = 1) +
-  # geom_line() +
-  geom_jitter(width = 0.2) +
-  ylim(0, NA) +
-  geom_boxplot(aes(group = cut_width(timestamp, 86400/5))) + 
-  geom_smooth(method="loess", level = 0.99999)
+# =================== main ==================
+
+# ofile <- ".\\data\\tsensors.csv"
+# generate_field_data(ofile, back_days = 7, forward_days = 0)
+# plot_field_data(".\\data\\test_data.csv")
+# 
+# generate_weather_data(".\\data\\tweather.csv", back_days = 7, forward_days = 3)
+plot_weather_data(".\\data\\tweather.csv")
+
+stop()
+
 
 # ======================
 getMap <- get_map(
