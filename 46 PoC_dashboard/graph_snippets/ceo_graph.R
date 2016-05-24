1bd43a7bdddefeb117bbb320973684a8d1055e41
iMissile <ishutov@gmail.com>
2016-05-24 10:58:16 +0300

    перехожу к настройке джобов

--- 46 PoC_dashboard/graph_snippets/ceo_graph.R
+++ 46 PoC_dashboard/graph_snippets/ceo_graph.R
@@ -23,89 +23,95 @@ library(gridExtra) # для grid.arrange()
 source("common_funcs.R") # сюда выносим все вычислительные и рисовательные функции
 
 
-getwd()
-
-# получаем исторические данные по погоде из репозитория Гарика --------------------------------------------------------
-# https://cran.r-project.org/web/packages/curl/vignettes/intro.html
-req <- curl_fetch_memory("https://raw.githubusercontent.com/iot-rus/Moscow-Lab/master/weather.txt")
-wrecs <- rawToChar(req$content) # weather history
-# wh_json <- gsub('\\\"', "'", txt, perl = TRUE) 
-# заменим концы строк на , и добавим шапочку и окончание для формирования семантически правильного json
-# последнюю ',' надо удалить, может такое встретиться (перевод строки)
-tmp <- paste0('{"res":[', gsub("\\n", ",\n", wrecs, perl = TRUE), ']}')
-wh_json <- gsub("},\n]}", "}]}", tmp)
-# t <- cat(wh_json)
-write(wh_json, file="./export/wh_json.txt")
-data <- fromJSON(wh_json)
-
-whist.df <- data$res$main
-whist.df$timestamp <- data$res$dt
+get_weather_df <- function(back_days = 7, forward_days = 3) {
+  # получаем исторические данные по погоде из репозитория Гарика --------------------------------------------------------
+  # https://cran.r-project.org/web/packages/curl/vignettes/intro.html
+  req <- curl_fetch_memory("https://raw.githubusercontent.com/iot-rus/Moscow-Lab/master/weather.txt")
+  wrecs <- rawToChar(req$content) # weather history
+  # wh_json <- gsub('\\\"', "'", txt, perl = TRUE) 
+  # заменим концы строк на , и добавим шапочку и окончание для формирования семантически правильного json
+  # последнюю ',' надо удалить, может такое встретиться (перевод строки)
+  tmp <- paste0('{"res":[', gsub("\\n", ",\n", wrecs, perl = TRUE), ']}')
+  wh_json <- gsub("},\n]}", "}]}", tmp)
+  # t <- cat(wh_json)
+  write(wh_json, file="./export/wh_json.txt")
+  data <- fromJSON(wh_json)
   
-# t0 <- '{"coord":{"lon":37.61,"lat":55.76},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"base":"cmc stations","main":{"temp":291.77,"pressure":1012,"humidity":72,"temp_min":290.15,"temp_max":295.35},"wind":{"speed":4,"deg":340},"clouds":{"all":0},"dt":1464008912,"sys":{"type":1,"id":7323,"message":0.0031,"country":"RU","sunrise":1463965411,"sunset":1464025820},"id":524894,"name":"Moskva","cod":200}'
-# t1 <- '{"coord":{"lon":37.61,"lat":55.76},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"base":"stations","main":{"temp":291.01,"pressure":1012,"humidity":72,"temp_min":289.15,"temp_max":292.15},"visibility":10000,"wind":{"speed":4,"deg":330},"clouds":{"all":0},"dt":1464007798,"sys":{"type":1,"id":7323,"message":0.0354,"country":"RU","sunrise":1463965412,"sunset":1464025819},"id":524894,"name":"Moskva","cod":200}'
-# t <- paste0('{"results":[', t0, ',', t1, ']}')
-# mdata <- fromJSON(t)
-
-# head(wh_json)
-
-# получаем прогноз через API --------------------------------------------------------
-url <- "api.openweathermap.org/data/2.5/"   
-MoscowID <- '524901'
-APPID <- '19deaa2837b6ae0e41e4a140329a1809'
-# reqstring <- paste0(url, "weather?id=", MoscowID, "&APPID=", APPID)
-reqstring <- paste0(url, "forecast?id=", MoscowID, "&APPID=", APPID) 
-resp <- GET(reqstring)
-if(status_code(resp) == 200){
-  r <- content(resp)
+  whist.df <- data$res$main
+  whist.df$timestamp <- data$res$dt
+    
+  # t0 <- '{"coord":{"lon":37.61,"lat":55.76},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"base":"cmc stations","main":{"temp":291.77,"pressure":1012,"humidity":72,"temp_min":290.15,"temp_max":295.35},"wind":{"speed":4,"deg":340},"clouds":{"all":0},"dt":1464008912,"sys":{"type":1,"id":7323,"message":0.0031,"country":"RU","sunrise":1463965411,"sunset":1464025820},"id":524894,"name":"Moskva","cod":200}'
+  # t1 <- '{"coord":{"lon":37.61,"lat":55.76},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"base":"stations","main":{"temp":291.01,"pressure":1012,"humidity":72,"temp_min":289.15,"temp_max":292.15},"visibility":10000,"wind":{"speed":4,"deg":330},"clouds":{"all":0},"dt":1464007798,"sys":{"type":1,"id":7323,"message":0.0354,"country":"RU","sunrise":1463965412,"sunset":1464025819},"id":524894,"name":"Moskva","cod":200}'
+  # t <- paste0('{"results":[', t0, ',', t1, ']}')
+  # mdata <- fromJSON(t)
+  
+  # head(wh_json)
+  
+  # получаем прогноз через API --------------------------------------------------------
+  url <- "api.openweathermap.org/data/2.5/"   
+  MoscowID <- '524901'
+  APPID <- '19deaa2837b6ae0e41e4a140329a1809'
+  # reqstring <- paste0(url, "weather?id=", MoscowID, "&APPID=", APPID)
+  reqstring <- paste0(url, "forecast?id=", MoscowID, "&APPID=", APPID) 
+  resp <- GET(reqstring)
+  if(status_code(resp) == 200){
+    r <- content(resp)
+  }
+  
+  # получаем погодные данные
+  m <- r$list
+  ll <- lapply(m, function(x){ 
+    ldate <- getElement(x, 'main')
+    ldate$timestamp <- getElement(x, 'dt')
+    ldate
+    })
+  l2 <- melt(ll)
+  # нормализуем под колонки, которые есть в исторических данных
+  l3 <- tidyr::spread(l2, L2, value) %>% 
+    select(-L1, -temp_kf) %>%
+    mutate(timestamp = as.integer(timestamp))
+  
+  # объединяем и вычищаем --------------------------------------------------------
+  
+  weather.df <- bind_rows(whist.df, l3) %>%
+    select(-temp_max, -temp_min, -sea_level, -grnd_level) %>%
+    distinct() %>% # удаляем дубли, которые навыдавал API
+    mutate(temp = round(temp - 273, 1)) %>% # пересчитываем из кельвинов в градусы цельсия
+    mutate(pressure = round(pressure * 0.75006375541921, 0)) %>% # пересчитываем из гектопаскалей (hPa) в мм рт. столба
+    mutate(humidity = round(humidity, 0)) %>%
+    mutate(timestamp = as.POSIXct(timestamp, origin='1970-01-01')) %>%
+    mutate(timegroup = hgroup.enum(timestamp, time.bin = 1)) # сделаем почасовую группировку
+    
+  # разметим данные на прошлое и будущее. будем использовать для цветовой группировки
+  weather.df['time.pos'] <- ifelse(weather.df$timestamp < now(), "PAST", "FUTURE")
+  
+  # причешем данные для графика у Паши + проведем усреднение по часовым группам
+  outw.df <- weather.df %>%
+    filter(timegroup >= floor_date(now() - days(back_days), unit = "day")) %>%
+    filter(timegroup <= ceiling_date(now() + days(forward_days), unit = "day")) %>%
+    group_by(timegroup, time.pos) %>%
+    summarise(temp = mean(temp), pressure = mean(pressure), humidity = mean(humidity)) %>%
+    ungroup
+  
+  # чтобы график не был разорванным, надо продублировать максимальную точку из PAST в группу FUTURE
+  POI.df <- outw.df %>%
+    filter(time.pos == 'PAST') %>%
+    filter(timegroup == max(timegroup)) %>%
+    mutate(time.pos = 'FUTURE')
+  
+  outw.df <- outw.df %>%
+    bind_rows(POI.df) %>%
+    arrange(timegroup)
+  
+  outw.df
 }
 
-# получаем погодные данные
-m <- r$list
-ll <- lapply(m, function(x){ 
-  ldate <- getElement(x, 'main')
-  ldate$timestamp <- getElement(x, 'dt')
-  ldate
-  })
-l2 <- melt(ll)
-# нормализуем под колонки, которые есть в исторических данных
-l3 <- tidyr::spread(l2, L2, value) %>% 
-  select(-L1, -temp_kf) %>%
-  mutate(timestamp = as.integer(timestamp))
 
-# объединяем и вычищаем --------------------------------------------------------
-
-weather.df <- bind_rows(whist.df, l3) %>%
-  select(-temp_max, -temp_min, -sea_level, -grnd_level) %>%
-  distinct() %>% # удаляем дубли, которые навыдавал API
-  mutate(temp = round(temp - 273, 1)) %>% # пересчитываем из кельвинов в градусы цельсия
-  mutate(pressure = round(pressure * 0.75006375541921, 0)) %>% # пересчитываем из гектопаскалей (hPa) в мм рт. столба
-  mutate(humidity = round(humidity, 0)) %>%
-  mutate(timestamp = as.POSIXct(timestamp, origin='1970-01-01')) %>%
-  mutate(timegroup = hgroup.enum(timestamp, time.bin = 1)) # сделаем почасовую группировку
-  
-# разметим данные на прошлое и будущее. будем использовать для цветовой группировки
-weather.df['time.pos'] <- ifelse(weather.df$timestamp < now(), "PAST", "FUTURE")
-
-# причешем данные для графика у Паши + проведем усреднение по часовым группам
-outw.df <- weather.df %>%
-  filter(timegroup >= floor_date(now() - days(7), unit = "day")) %>%
-  filter(timegroup <= ceiling_date(now() + days(3), unit = "day")) %>%
-  group_by(timegroup, time.pos) %>%
-  summarise(temp = mean(temp), pressure = mean(pressure), humidity = mean(humidity)) %>%
-  ungroup
-
-# чтобы график не был разорванным, надо продублировать максимальную точку из PAST в группу FUTURE
-POI.df <- outw.df %>%
-  filter(time.pos == 'PAST') %>%
-  filter(timegroup == max(timegroup)) %>%
-  mutate(time.pos = 'FUTURE')
-
-outw.df <- outw.df %>%
-  bind_rows(POI.df) %>%
-  arrange(timegroup)
-
+getwd()
+df <- get_weather_df(back_days = 7, forward_days = 3)
 
 # сделаем выгрузку в json --------------------------------------------------------
+
 # df2 <- data.frame(timestamp = round(as.numeric(outw.df$timegroup), 0), 
 #                   air_temp_past = ifelse(outw.df$time.pos == "PAST", round(outw.df$temp, 1), NA),
 #                   air_temp_future = ifelse(outw.df$time.pos == "FUTURE", round(outw.df$temp, 1), NA),
@@ -113,7 +119,7 @@ outw.df <- outw.df %>%
 #                   air_humidity_future = ifelse(outw.df$time.pos == "FUTURE", round(outw.df$humidity, 1), NA)) %>%
 #   arrange(timestamp)
 
-df3 <- with(outw.df, {
+df3 <- with(df, {
   data.frame(timestamp = round(as.numeric(timegroup), 0), 
                   air_temp_past = ifelse(time.pos == "PAST", round(temp, 1), NA),
                   air_temp_future = ifelse(time.pos == "FUTURE", round(temp, 1), NA),
@@ -131,13 +137,16 @@ write(x, file="./export/real_weather.json")
 p1 <- ggplot(outw.df, aes(timegroup, temp, colour = time.pos)) +
   # ggtitle("График температуры") +
   # scale_fill_brewer(palette="Set1") +
-  scale_fill_brewer(palette = "Paired") +
+  # scale_fill_brewer(palette = "Paired") +
+  scale_color_brewer(palette = "Paired") +
   # geom_ribbon(aes(ymin = temp.min, ymax = temp.max, fill = time.pos), alpha = 0.5) +
   # geom_point(shape = 1, size = 3) +
   # geom_line(lwd = 1, linetype = 'dashed', color = "red") +
-  scale_x_datetime(breaks = date_breaks("1 days"), minor_breaks = date_breaks("6 hours")) +
+  scale_x_datetime(labels = date_format("%d.%m"), breaks = date_breaks("1 days"), minor_breaks = date_breaks("6 hours")) +
   geom_line(lwd = 1.2) +
-  # theme_igray() +
-  theme(legend.position="none")
+  theme_igray() +
+  theme(legend.position="none") +
+  xlab("Дата") +
+  ylab("Температура, град. C")
 
 p1
---
+++ 46 PoC_dashboard/graph_snippets/synthetic_data_hc.R
@@ -0,0 +1,118 @@
+rm(list=ls()) # очистим все переменные
+
+library(ggplot2) #load first! (Wickham)
+library(lubridate) #load second!
+library(dplyr)
+library(readr)
+library(jsonlite)
+library(magrittr)
+library(futile.logger)
+
+
+generate_field_data <- function(back_days = 7, forward_days = 7) {
+  # данные по расположению сенсоров
+  sensor <- data.frame(
+    c(1, 37.578691607470724, 55.766160765720493),
+    c(2, 37.57990362015564, 55.766504551149772),
+    c(3, 37.580025839922193, 55.765874275547077),
+    c(4, 37.577632369493983, 55.764819973580607),
+    c(5, 37.581431367237478, 55.764384492709446),
+    c(6, 37.579109191673091, 55.765158040903287),
+    c(7, 37.58182858147876, 55.766160765720493),
+    c(8, 37.580586013852198, 55.767008764295831),
+    c(9, 37.579862880233463, 55.764109449653162),
+    c(10, 37.577856439065989, 55.766567578149633),
+    c(11, 37.576776831128157, 55.76609773806296)
+  )
+  # sensor <- t(sensor) #транспонируем
+  # sensor <- tFrame(sensor) #транспонируем
+  sensor <- as.data.frame(t(sensor))
+  rownames(sensor) <- NULL
+  colnames(sensor) <- c("name", "lon", "lat") # http://stackoverflow.com/questions/6081439/changing-column-names-of-a-data-frame-in-r
+  
+  #sensor <- as.data.frame(t(sensor)) #транспонируем
+  # dimnames(sensor) <- NULL
+  
+  # генерируем данные по показани€м сенсоров (почасова€ раскладка)
+  # tick.seq <- seq(as.POSIXct("2016-03-01 23:00:00"), as.POSIXct("2016-03-10 08:32:00"), by = "4 hours") # http://stackoverflow.com/questions/10887923/hourly-date-sequence-in-r
+  start_time <- round_date(now(), unit = "day") # дл€ синхронных 4-х часовых измерений необходимо привести к 0:00
+  tick.seq <- seq(start_time - days(back_days), start_time + days(forward_days), by = "4 hours") # http://stackoverflow.com/questions/10887923/hourly-date-sequence-in-r
+  # собираем сразу data.frame: врем€, #сенсора, показание
+  # генерируем показатели влажности, допустимый диапазон [0; 100]
+  n <- length(tick.seq)
+  mydata <- data.frame(name = rep(sensor$name, each = n),
+                       lon = rep(sensor$lon, each = n),
+                       lat = rep(sensor$lat, each = n),
+                       type = "soil_moisture",
+                       location = "Moscow Lab",
+                       #location = " артофель 1",
+                       timestamp = tick.seq, 
+                       value = rescale(rnorm(nrow(sensor) * n, 70, 30), to = c(50, 80)))
+  mydata <- mydata %>%
+    mutate(min = value * runif(nrow(sensor) * n, 0.1, 0.9)) %>%
+    mutate(max = value * runif(nrow(sensor) * n, 1.1, 1.5)) %>%
+    mutate(max = ifelse(max <= 100, max, 100))
+    
+  # rnorm(1000, 3, .25) # Generates 1000 numbers from a normal with mean 3 and sd=.25
+  
+  # дл€ соотв. реали€м сделаем разброс во времени измерени€
+  # mydata$timestamp <- mydata$timestamp + runif(nrow(mydata), min = -5*60, max = 5*60)
+  mydata$value <- round(mydata$value + 0.2*sin(as.numeric(mydata$timestamp)/86400*(0.1*pi)), 1)
+  
+  mydata
+}
+
+
+flog.appender(appender.file("./log/iot.log"))
+flog.threshold(TRACE)
+flog.info("Job started")
+flog.info("Working directory: %s", getwd())
+
+flog.info("Processing started")
+df <- generate_field_data(back_days = 7, forward_days = 3)
+qplot(timestamp, value, data = df)
+# http://stackoverflow.com/questions/25550711/convert-data-frame-to-json
+x <- jsonlite::toJSON(df, pretty = TRUE)
+write(x, file="./export/sample.json")
+flog.info("Job finished")
+
+
+# сгеренируем json под требовани€ ѕаши ---------------------------------------------------
+# http://arxiv.org/pdf/1403.2805v1.pdf  |   http://arxiv.org/abs/1403.2805
+df2 <- data.frame(timestamp = round(as.numeric(df$timestamp), 0), 
+                  soil_moisture = round(df$value, 1),
+                  soil_moisture_min = round(df$min, 1), 
+                  soil_moisture_max = round(df$max, 1)) %>%
+  arrange(timestamp)
+
+x <- jsonlite::toJSON(list(results = df2), pretty = TRUE)
+write(x, file="./export/sample_highcharts.json")
+
+
+stop() # изыскани€
+# сгеренируем json под требовани€ ѕаши ---------------------------------------------------
+# http://arxiv.org/pdf/1403.2805v1.pdf  |   http://arxiv.org/abs/1403.2805
+options(stringsAsFactors = FALSE)
+df1 <- data.frame(results = rep('name', nrow(df)))
+df2 <- data.frame(timestamp = round(as.numeric(df$timestamp), 0), 
+                  soil_moisture = round(df$value, 1),
+                  soil_moisture_min = round(df$min, 1), 
+                  soil_moisture_max = round(df$max, 1)) %>%
+  arrange(timestamp)
+
+# ќб€зательно надо уровн€ть количество элементов, иначе получаем ошибку: 
+# Error in `$<-.data.frame`(`*tmp*`, "results", value = list(timestamp = c(1463345834,  : replacement has 671 rows, data has 1
+df1$results <- df2 
+
+ll <- list(results = df2)
+
+# df3 <- data.frame(results = df2) # так встроенный data.frame не получим
+
+gp <- ggplot(df1$results, aes(timestamp, soil_moisture)) +
+  geom_line()
+gp
+
+x <- jsonlite::toJSON(ll, pretty = TRUE)
+write(x, file="./export/sample_highcharts.json")
+
+
