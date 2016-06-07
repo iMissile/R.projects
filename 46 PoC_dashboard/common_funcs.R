a24ca5e14df8d2aeb73fb1aba8ca8ad5497ae644
iMissile <ishutov@gmail.com>
2016-06-07 10:52:28 +0300

    Исправил проблему в выборе временного промежутка

--- 46 PoC_dashboard/common_funcs.R
+++ 46 PoC_dashboard/common_funcs.R
@@ -3,9 +3,9 @@ get_timeframe <- function(days_back = 7, days_forward = 3){
   # если по каким-либо причинам наверху не определились с прогнозом (NA), 
   # то полагаем что он есть и он равен базовому горизонту
   days_formard <- ifelse(is.na(days_forward), 3, days_forward)
-  min_lim <- ceiling_date(now() - days(days_back), unit = "day")
+  min_lim <- floor_date(now() - days(days_back), unit = "day")
   # поскольку будущее округляем вниз, то надо добавить еще сутки (+1)
-  max_lim <- floor_date(now() + days(days_forward + 1), unit = "day") 
+  max_lim <- ceiling_date(now() + days(days_forward), unit = "day") 
   timeframe <- c(min_lim, max_lim)
   
   timeframe
