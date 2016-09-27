75a632fa5466e8e2d414cfcf2aee3b1e6deb49b3
iMissile <ishutov@gmail.com>
2016-09-27 14:01:27 +0300

    Ответ в Яндекс

--- 52 yandex_taxi/analysis.R
+++ 52 yandex_taxi/analysis.R
@@ -19,42 +19,44 @@ source("funcs.R") # сюда выносим все вычислительные и рисовательные функции
 # rawdata <- read_excel("./src/tv_sample.xlsx", sheet = 1, col_names = TRUE, col_types =)
 
 # проверим, как распознаются даты с явным указанием таймзон
-dmy_hms('19.01.2015 17:52:51Z+00', truncated = 3, tz = 'Europe/Moscow')
+tdate <- dmy_hms("19.01.2015 17:52:51Z+00", truncated = 3, tz = "Europe/Moscow")
+tdate
+with_tz(tdate, tz = "Asia/Yekaterinburg")
 
 # определяем timezone
 # http://stackoverflow.com/questions/36300381/dplyr-mutate-with-function-call-returning-incorrect-value
 # http://unicode.org/repos/cldr/trunk/common/supplemental/windowsZones.xml
 get_tz <- function(m) {
-  case_when(m == 'Москва' ~ 'Europe/Moscow',
-            m == 'Пермь' ~ 'Asia/Yekaterinburg')
+  case_when(m == "Москва" ~ "Europe/Moscow",
+            m == "Пермь" ~ "Asia/Yekaterinburg")
 }
 
 # так мы обходим то, что dmy не принимает tz в виде вектора
 get_tz_offset <- function(m) {
-  case_when(m == 'Москва' ~ '+03',
-            m == 'Пермь' ~ '+05')
+  case_when(m == "Москва" ~ "+03",
+            m == "Пермь" ~ "+05")
 }
 
 # http://barryrowlingson.github.io/hadleyverse/#5
 # сразу преобразуем время
 raw_tv.df <- read_delim(
-  './data/tv.csv', delim = ";", quote = "\"",
+  "./data/tv.csv", delim = ";", quote = "\"",
   col_names = TRUE,
-  col_types = str_c(rep('c', 18), collapse = ''),
+  col_types = str_c(rep("c", 18), collapse = ""),
   locale = locale("ru", encoding = "windows-1251", tz = "Europe/Moscow"), # таймзону, в принципе, можно установить здесь
   progress = interactive()
 )
 
 raw_installs.df <- read_delim(
-  './data/installs.csv', delim = ";", quote = "\"",
-  col_types = 'cci',
+  "./data/installs.csv", delim = ";", quote = "\"",
+  col_types = "cci",
   locale = locale("ru", encoding = "windows-1251", tz = "Europe/Moscow"), # таймзону, в принципе, можно установить здесь
   progress = interactive()
 )
 
 raw_visits.df <- read_delim(
-  './data/visits.csv', delim = ";", quote = "\"",
-  col_types = 'cciii',
+  "./data/visits.csv", delim = ";", quote = "\"",
+  col_types = "cciii",
   locale = locale("ru", encoding = "windows-1251", tz = "Europe/Moscow"), # таймзону, в принципе, можно установить здесь
   progress = interactive()
 )
@@ -74,41 +76,55 @@ t3 <- map(raw_visits.df, unique)
 
 # ===== приведем время в порядок, можно подумать насчет устранения copy-paste
 raw_tv.df %<>%
-  mutate(timestamp = dmy_hms(str_c(local_date, local_time, 'Z', get_tz_offset(city), sep = ' '), 
-                             truncated = 3, tz = 'Europe/Moscow')) 
+  mutate(timestamp = dmy_hms(str_c(local_date, local_time, "Z", get_tz_offset(city), sep = " "), 
+                             truncated = 3, tz = "Europe/Moscow")) 
 raw_installs.df %<>%
-  mutate(timestamp = dmy_hms(moscow_time, truncated = 3, tz = 'Europe/Moscow')) 
+  mutate(timestamp = dmy_hms(moscow_time, truncated = 3, tz = "Europe/Moscow")) 
 raw_visits.df %<>%
-  mutate(timestamp = dmy_hms(moscow_time, truncated = 3, tz = 'Europe/Moscow'))
+  mutate(timestamp = dmy_hms(moscow_time, truncated = 3, tz = "Europe/Moscow"))
 
 #  select(city, local_time, local_date, timestamp)
 
 # ===== попробуем взглянуть на данные
-s_date <- dmy_hm("25.01.2015 0:0", tz="Europe/Moscow")
-e_date <- s_date + days(3)
+# получается гребенка, надо усреднить
+
+s_date <- dmy_hm("18.01.2015 0:0", tz="Europe/Moscow")
+e_date <- s_date + days(60)
+
+dst <- "Пермь"
+dst <- "Москва"
+# http://stackoverflow.com/questions/24569154/use-variable-names-in-functions-of-dplyr
 
 subdata <- raw_installs.df %>%
-  filter(city == 'Пермь') %>%
+  filter_(str_c("city == '", dst, "'")) %>%
+  mutate(timestamp = dmy_hms(moscow_time, truncated = 3, tz = "Europe/Moscow")) %>%
+  #mutate(timestamp2 = lubridate::with_tz(timestamp, tzone = 'Asia/Yekaterinburg')) %>%
+  # mutate_(timestamp = str_c("lubridate::with_tz(timestamp, tzone = '", get_tz(dst), "')")) %>% # работает
   filter(timestamp > s_date & timestamp < e_date) %>%
-  mutate(timegroup = hgroup.enum(timestamp, time.bin = 0.5)) %>%
+  mutate(timegroup = hgroup.enum(timestamp, time.bin = 1)) %>%
   # суммируем все инсталляции
   group_by(timegroup) %>%
   summarise(inst = sum(installations))
 
-subdata
-
+tv.df <- raw_tv.df %>% 
+  select(timestamp, city) %>%
+  filter_(str_c("city == '", dst, "'")) %>%
+  filter(timestamp > s_date & timestamp < e_date) %>%
+  mutate(stub = 1000)
 
 gp <- ggplot(subdata, aes(x = timegroup, y = inst)) +
-  geom_point(size=2, shape=21) + # produce scatterplot
+  geom_point(size = 2, shape = 21) + # produce scatterplot
   geom_line(lwd = 1) +
-  scale_x_datetime(labels = date_format_tz("%d.%m %H:%M", tz="Europe/Moscow"), 
+  scale_x_datetime(labels = date_format_tz("%d.%m %H:%M", tz = get_tz(dst)),
+  #scale_x_datetime(labels = date_format_tz("%d.%m %H:%M", tz = "Europe/Moscow"), 
                    breaks = date_breaks("4 hours"), 
                    minor_breaks = date_breaks("30 mins")) + 
-  theme_bw()
+  # добавим сюда данные по показу рекламы
+  geom_point(data = tv.df, mapping = aes(x = timestamp, y = stub), size = 1, color = "red") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
 gp
 
-
-# получается гребенка, надо усреднить
 
 stop()
 
