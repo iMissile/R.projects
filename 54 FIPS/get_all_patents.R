b6e0efc790971f7d40635e53a3fa4379fac7f00e
iMissile <ishutov@gmail.com>
2016-11-09 09:38:14 +0300

    Перехожу на httr

--- 54 FIPS/get_all_patents.R
+++ 54 FIPS/get_all_patents.R
@@ -2,7 +2,9 @@ library(ggplot2) #load first! (Wickham)
 library(lubridate) #load second!
 library(dplyr)
 library(tidyr)
+library(tibble)
 library(readr)
+library(stringi)
 library(stringr)
 library(jsonlite)
 library(magrittr)
@@ -18,6 +20,23 @@ library(future)
 library(microbenchmark)
 library(futile.logger)
 
+
+# ==== functions definition
+getAttr <- function(text, attrName) {
+  res <- html_nodes(text, xpath=paste0("//div[@class='", attrName, "']")) %>% 
+    extract(-1) %>% # magrittr::extract(-1), удалили первую строку, там заголовок таблицы
+    # magrittr provides a series of aliases which can be more pleasant to use when composing chains using the %>% operator
+    html_text() # взяли сам атрибут
+  
+  j <- res[[1]]
+  stri_encode(j, from="UTF-8", to="windows-1251", to_raw = FALSE)
+  browser()
+  # iconv(from = "UTF8", to = "windows-1251")
+  # m %>% html_nodes('div')
+  res
+}
+
+# ======
 flog.appender(appender.file("FIPS.log"))
 flog.threshold(TRACE)
 flog.info("============= Parsing started ===============")
@@ -34,35 +53,51 @@ req_str1 <- "http://www1.fips.ru/wps/portal/!ut/p/c5/jY7LDoIwFES_hS-4l2dhWYhpC4h
 req_str2 <- "&selectedDBs=RUPATABRU%3BRUPATAP%3BRUPAT_NEW%3BRUPMAB%3BRUPM_NEW%3BIMPIN&fromUserId=514"
 
 all_docs <-
-  foreach(n = iter(0:0), .packages = 'futile.logger', .combine = "c") %do% {
-            url <- str_c(req_str1, n, req_str2, collapse = "")
-            resp <- try(curl_fetch_memory(url))
-
-            # проверим только 1-ый элемент класса, поскльку при разных ответах получается разное кол-во элементов
-            if (class(resp)[[1]] == "try-error" || resp$status_code != 200) {
-              # http://stackoverflow.com/questions/15595478/how-to-get-the-name-of-the-calling-function-inside-the-called-routine
-              flog.error(paste0("Error in ", calledFun, " called from ", callingFun, ". Class(resp) = ", class(resp)))
-              flog.error(paste0("resp = ", resp))
-              # сигнализируем о невозможности обновить данные
-              elem <- NULL
-            } else {
-              # проводим обработку контента
-              flog.info(paste0("Parsing page ", n))
-              htext <- fromJSON(rawToChar(resp$content))
-              
-              elem <- htext$result$SearchResult$hitList
-              m <- read_html(elem, options=c("RECOVER"))
-              # теперь собираем все по частям в data.frame
-              dvIndex <- html_nodes(m, xpath="//div[@class='dvIndex']") %>% 
-                magrittr::extract(-1) %>% # удалили первую строку, там заголовок таблицы
-                html_text()
-              #  iconv(from = "UTF8", to = "windows-1251")
-#              browser()
-#              m %>% html_nodes('div')
-            }
-            
-            elem
+  foreach(n = iter(0:0), .packages = 'futile.logger', .combine = rbind) %do% {
+    url <- str_c(req_str1, n, req_str2, collapse = "")
+    resp <- try(curl_fetch_memory(url))
+    # переходим на httr: https://cran.r-project.org/web/packages/httr/vignettes/quickstart.html
+    # обработку exception пока не проводим
+    resp <- GET(url)
+    
+    # проводим обработку контента
+    flog.info(paste0("Parsing page ", n, " HTTP Status Code = ", resp$status_code))
+    htext <- fromJSON(rawToChar(resp$content))
+    # browser()
+    
+    ht <- htext$result$SearchResult$hitList
+    j3 <- stri_encode(ht, from = "UTF-8", to = "cp1251")
+    browser()
+    m <- read_html(ht, encoding = "UTF-8")
+    m2 <- read_html(ht, encoding = "windows-1251")
+    guess_encoding(m)
+    browser()
+    
+    
+    # теперь собираем все по частям в data.frame
+    dvIndex <-
+      getAttr(m, "dvIndex") %>% stri_replace_all_fixed(".", "") # убрали точку после номера
+    dvNumDoc <- getAttr(m, "dvNumDoc")
+    dvDatePubl <- getAttr(m, "dvDatePubl")
+    dvTitle <- getAttr(m, "dvTitle")
+    
+    # browser()
+    elem <- tibble(
+      dvIndex = as.numeric(dvIndex),
+      dvNumDoc = dvNumDoc,
+      dvDatePubl = dvDatePubl,
+      dvTitle = iconv(dvTitle, from = "UTF8", to = "windows-1251")
+    )
+    # Encoding(dvTitle)
+    
+    elem
   }
 
-write(all_docs, file="out.txt", append=FALSE)
+
+write_csv(all_docs, "out.csv", append=FALSE)
 flog.info("Output file generated")
+
+stop()
+
+m <- iconv(dvTitle, from="UTF8", to="windows-1251")
+j3 <- stri_encode(j, from="UTF-8", to="cp1251")
