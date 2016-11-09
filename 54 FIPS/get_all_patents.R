ec8a94e4c8a88102e3602009e0ed2dc1fea9cbb1
iMissile <ishutov@gmail.com>
2016-11-09 09:58:01 +0300

    ура!! парсинг с правильной кодировкой заработал

--- 54 FIPS/get_all_patents.R
+++ 54 FIPS/get_all_patents.R
@@ -28,9 +28,9 @@ getAttr <- function(text, attrName) {
     # magrittr provides a series of aliases which can be more pleasant to use when composing chains using the %>% operator
     html_text() # взяли сам атрибут
   
-  j <- res[[1]]
-  stri_encode(j, from="UTF-8", to="windows-1251", to_raw = FALSE)
-  browser()
+  # j <- res[[1]]
+  # stri_encode(j, from="UTF-8", to="windows-1251", to_raw = FALSE)
+  # browser()
   # iconv(from = "UTF8", to = "windows-1251")
   # m %>% html_nodes('div')
   res
@@ -53,7 +53,7 @@ req_str1 <- "http://www1.fips.ru/wps/portal/!ut/p/c5/jY7LDoIwFES_hS-4l2dhWYhpC4h
 req_str2 <- "&selectedDBs=RUPATABRU%3BRUPATAP%3BRUPAT_NEW%3BRUPMAB%3BRUPM_NEW%3BIMPIN&fromUserId=514"
 
 all_docs <-
-  foreach(n = iter(0:0), .packages = 'futile.logger', .combine = rbind) %do% {
+  foreach(n = iter(0:74), .packages = 'futile.logger', .combine = rbind) %do% {
     url <- str_c(req_str1, n, req_str2, collapse = "")
     resp <- try(curl_fetch_memory(url))
     # переходим на httr: https://cran.r-project.org/web/packages/httr/vignettes/quickstart.html
@@ -62,21 +62,21 @@ all_docs <-
     
     # проводим обработку контента
     flog.info(paste0("Parsing page ", n, " HTTP Status Code = ", resp$status_code))
-    htext <- fromJSON(rawToChar(resp$content))
+    
+    htext <- fromJSON(content(resp, "text"))
     # browser()
     
     ht <- htext$result$SearchResult$hitList
-    j3 <- stri_encode(ht, from = "UTF-8", to = "cp1251")
-    browser()
+    # j3 <- stri_encode(ht, from = "UTF-8", to = "cp1251")
+    # browser()
     m <- read_html(ht, encoding = "UTF-8")
-    m2 <- read_html(ht, encoding = "windows-1251")
-    guess_encoding(m)
-    browser()
+    # m2 <- read_html(ht, encoding = "windows-1251")
+    # guess_encoding(m)
+    # browser()
     
     
     # теперь собираем все по частям в data.frame
-    dvIndex <-
-      getAttr(m, "dvIndex") %>% stri_replace_all_fixed(".", "") # убрали точку после номера
+    dvIndex <- getAttr(m, "dvIndex") %>% stri_replace_all_fixed(".", "") # убрали точку после номера
     dvNumDoc <- getAttr(m, "dvNumDoc")
     dvDatePubl <- getAttr(m, "dvDatePubl")
     dvTitle <- getAttr(m, "dvTitle")
@@ -86,7 +86,7 @@ all_docs <-
       dvIndex = as.numeric(dvIndex),
       dvNumDoc = dvNumDoc,
       dvDatePubl = dvDatePubl,
-      dvTitle = iconv(dvTitle, from = "UTF8", to = "windows-1251")
+      dvTitle = dvTitle
     )
     # Encoding(dvTitle)
     
