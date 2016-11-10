78ae5e363bbf706db0a6e37f0818b6ee7aaf61c7
iMissile <ishutov@gmail.com>
2016-11-10 16:50:10 +0300

    начал разбор тела патентов

--- 54 FIPS/enreach_all_patents.R
+++ 54 FIPS/enreach_all_patents.R
@@ -1,3 +1,4 @@
+rm(list=ls()) # очистим все переменные
 library(ggplot2) #load first! (Wickham)
 library(lubridate) #load second!
 library(dplyr)
@@ -40,22 +41,24 @@ getAttr <- function(text, attrName) {
 # ======
 flog.appender(appender.file("FIPS.log"))
 flog.threshold(TRACE)
-flog.info("============= Parsing started ===============")
+flog.info("============= Enreachment started ===============")
 
-# точка захода (от Ѕрыкова): http://www1.fips.ru/wps/portal/IPS_Ru#1478591840047
 
-# магический url дл€ считывани€ части (50 шт) документов по ћѕ  = G05B вз€та из дебага в браузере и выгл€дит следующим образом:
-# http://www1.fips.ru/wps/portal/!ut/p/c5/jY7LDoIwFES_hS-4l2dhWYhpC4hgYhA2pCENYngYVBZ-vbByJTqznJyZgRIWD3JuG_lox0F2cIbSqYSgKY-Yjij2LlIvC0Pqu8h2-pIXThUwyi0SI7LkGKCwfMvgzDdQmP_Q-EUUf9D5-nZ7fc03-hM-9goKKMhn55AQD2lsRzpPXJN5NuSTuo_PqVaQ1bK-qFjNqktlo-DWn854JS9KNe0NbQg1dw!!/?beanMethod=doRestoreQuery&queryId=2737608&doSearch=true&pageNumber=2&selectedDBs=RUPATABRU%3BRUPATAP%3BRUPAT_NEW%3BRUPMAB%3BRUPM_NEW%3BIMPIN&fromUserId=514
-# где pagenumber мен€етс€ от 0 до 74 (всего документов 3743)
+# загружаем ранее собранный список патентов
+all_patents <- read_csv("patents_list_test.csv")
+
+# магический url дл€ считывани€ первой страницы документа из скачанного ранее списка с ћѕ  = G05B вз€та из дебага в браузере и выгл€дит следующим образом:
+# http://www1.fips.ru/wps/portal/!ut/p/c5/jY7LDoIwFES_hS-4l2dhWYhpC4hgYhA2pCENYngYVBZ-vbByJTqznJyZgRIWD3JuG_lox0F2cIbSqYSgKY-Yjij2LlIvC0Pqu8h2-pIXThUwyi0SI7LkGKCwfMvgzDdQmP_Q-EUUf9D5-nZ7fc03-hM-9goKKMhn55AQD2lsRzpPXJN5NuSTuo_PqVaQ1bK-qFjNqktlo-DWn854JS9KNe0NbQg1dw!!/?beanMethod=getDocument&queryId=2760601&documId=6b45832227654a2686e278babf036537&checkBoxes=&fromUserId=514
+# где documId берем из загруженной выше таблицы (всего найдено документов 3743)
 # в ответ получаем json, у которого в поле result -> hitlist лежит html со списком блока документов
 # http://www.jsoneditoronline.org/
 # http://codebeautify.org/jsonviewer
 
-req_str1 <- "http://www1.fips.ru/wps/portal/!ut/p/c5/jY7LDoIwFES_hS-4l2dhWYhpC4hgYhA2pCENYngYVBZ-vbByJTqznJyZgRIWD3JuG_lox0F2cIbSqYSgKY-Yjij2LlIvC0Pqu8h2-pIXThUwyi0SI7LkGKCwfMvgzDdQmP_Q-EUUf9D5-nZ7fc03-hM-9goKKMhn55AQD2lsRzpPXJN5NuSTuo_PqVaQ1bK-qFjNqktlo-DWn854JS9KNe0NbQg1dw!!/?beanMethod=doRestoreQuery&queryId=2737608&doSearch=true&pageNumber="
-req_str2 <- "&selectedDBs=RUPATABRU%3BRUPATAP%3BRUPAT_NEW%3BRUPMAB%3BRUPM_NEW%3BIMPIN&fromUserId=514"
+req_str1 <- "http://www1.fips.ru/wps/portal/!ut/p/c5/jY7LDoIwFES_hS-4l2dhWYhpC4hgYhA2pCENYngYVBZ-vbByJTqznJyZgRIWD3JuG_lox0F2cIbSqYSgKY-Yjij2LlIvC0Pqu8h2-pIXThUwyi0SI7LkGKCwfMvgzDdQmP_Q-EUUf9D5-nZ7fc03-hM-9goKKMhn55AQD2lsRzpPXJN5NuSTuo_PqVaQ1bK-qFjNqktlo-DWn854JS9KNe0NbQg1dw!!/?beanMethod=getDocument&queryId=2760601&documId="
+req_str2 <- "&checkBoxes=&fromUserId=514"
 
-all_patents <-
-  foreach(n = iter(0:0), .packages = 'futile.logger', .combine = rbind) %do% {
+all_docs <-
+  foreach(n=iter(all_patents$docID), .packages='futile.logger', .combine=rbind) %do% {
     ur1 <- str_c(req_str1, n, req_str2, collapse = "")
     # browser()
     # resp <- try(curl_fetch_memory(url))
@@ -64,12 +67,12 @@ all_patents <-
     resp <- GET(ur1)
     
     # проводим обработку контента
-    flog.info(paste0("Parsing page ", n, " HTTP Status Code = ", resp$status_code))
+    flog.info(paste0("Parsing documentId = ", n, " HTTP Status Code = ", resp$status_code))
     
     htext <- fromJSON(content(resp, "text"))
     # browser()
     
-    ht <- htext$result$SearchResult$hitList
+    ht <- htext$result$html
     # j3 <- stri_encode(ht, from = "UTF-8", to = "cp1251")
     # browser()
     m <- read_html(ht, encoding = "UTF-8")
@@ -78,24 +81,17 @@ all_patents <-
     # browser()
     
     
-    # теперь собираем все по част€м в data.frame
-    dvIndex <- getAttr(m, "dvIndex") %>% stri_replace_all_fixed(".", "") # убрали точку после номера
-    dvNumDoc <- getAttr(m, "dvNumDoc")
-    dvDatePubl <- getAttr(m, "dvDatePubl")
-    dvTitle <- getAttr(m, "dvTitle")
-    
+    # выцепл€ем ѕатентообладател€
+    # IPR <- html_nodes(m, xpath="//*[@id='bibl']/p[2]/b") %>% html_text()
+    tmp <- html_nodes(m, xpath="//*[@id='bibl']") %>% html_text()
+    IPR <- stri_match_first_regex(tmp, "\\(73\\) ѕатентообладатель\\(и\\):(.+?)\r\n")[[2]]
     # browser()
-    # выцепл€ем идентификатор документа
-    docID <- html_nodes(m, xpath="//a[@class='hitListRow']") %>% html_attr("id")
-    
+    flog.info(paste0("ѕатентообладатель = ", IPR))
+    # browser()
     
     # browser()
     elem <- tibble(
-      dvIndex=as.numeric(dvIndex),
-      dvNumDoc=dvNumDoc,
-      dvDatePubl=dvDatePubl,
-      docID=docID, 
-      dvTitle=dvTitle
+      IPR=IPR
     )
     # Encoding(dvTitle)
     
@@ -103,13 +99,19 @@ all_patents <-
   }
 
 
-write_csv(all_patents, "patents_list.csv", append=FALSE)
-flog.info("Output file generated")
+write_csv(all_docs, "out_enreach2.csv", append=FALSE)
+flog.info("Output file enreached")
+
+stop()
+# сниппеты
+stri_match_first_regex(IPR, "\\(73\\) ѕатентообладатель\\(и\\):(.+?)\r\n")
+
 
 stop()
 
 # === пробуем вытащить документ вручную по его id
 # пр€ма€ адресаци€ по ссылке: http://www1.fips.ru/wps/portal/!ut/p/c5/jY7LDoIwFES_hS-4l2dhWYhpC4hgYhA2pCENYngYVBZ-vbByJTqznJyZgRIWD3JuG_lox0F2cIbSqYSgKY-Yjij2LlIvC0Pqu8h2-pIXThUwyi0SI7LkGKCwfMvgzDdQmP_Q-EUUf9D5-nZ7fc03-hM-9goKKMhn55AQD2lsRzpPXJN5NuSTuo_PqVaQ1bK-qFjNqktlo-DWn854JS9KNe0NbQg1dw!!/?beanMethod=getDocument&queryId=2760601&documId=dae8d132a3b82a6abef5bab3de50c234&checkBoxes=&fromUserId=514
+write(ht, "resp.txt", append=FALSE)
 
 
 m <- iconv(dvTitle, from="UTF8", to="windows-1251")
