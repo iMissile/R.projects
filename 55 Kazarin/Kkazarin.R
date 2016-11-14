93ab3b13157c9c00d5581b8322e1ffd9d3e9f0c5
iMissile <ishutov@gmail.com>
2016-11-14 19:12:58 +0300

    теперь разбираюсь с NAs introduced by coercion в as.numeric

--- 55 Kazarin/Kkazarin.R
+++ 55 Kazarin/Kkazarin.R
@@ -36,21 +36,14 @@ getOksData <- function(filename, sheetname = "") {
   # названия колонок размазаны по строкам 1-3. 
   # 1-ая -- группирующая, 2-я -- детализирующая, 3-я -- единица измерения (в начале таблицы там имена колонок)
   # Надо их слить и переименовать колонки, причем приоритет имеет строка 3, как уточняющая
-  #name_c2 <- tidyr::gather(df0[1, ], key = name, value = name_c2) # 1-ая колонка ушла в имена
-  #name_c3 <- tidyr::gather(df0[2, ], key = name, value = name_c3) # 1-ая колонка ушла в имена
-  
-  # различные виды join не подойдут, поскольку мы хотим оставить все строки вне зависимости от результата
-  # сливать по именам опасно, вдруг есть дубли
-  # names.df <- dplyr::full_join(name_c2, name_c3, by = "name")
   names_df <- tibble(name_c1=tidyr::gather(df0[1, ], key=name, value=v)$v,
                      name_c2=tidyr::gather(df0[2, ], key=name, value=v)$v,
                      name_c3=tidyr::gather(df0[3, ], key=name, value=v)$v) %>%
     # http://www.markhneedham.com/blog/2015/06/28/r-dplyr-update-rows-with-earlierprevious-rows-values/
-   mutate(name_c1_ext=na.locf(name_c1, na.rm=FALSE)) %>% # в начале таблицы заголовки полей идут с 3-го уровня
-   mutate(name_c2_ext=na.locf(name_c2, na.rm=FALSE))
-  browser()
-  
-  t <- names_df %>% 
+    mutate(name_c1_ext=na.locf(name_c1, na.rm=FALSE)) %>% # в начале таблицы заголовки полей идут с 3-го уровня
+    mutate(name_c2_ext=na.locf(name_c2, na.rm=FALSE))
+
+  cnames <- names_df %>% 
     mutate(name.fix=str_c(str_replace_na(name_c1_ext, ""), # заменяем NA на "", см хелп на str_c
                           str_replace_na(name_c2_ext, ""), 
                           str_replace_na(name_c3, ""), sep = ":: ")) %>%
@@ -58,43 +51,42 @@ getOksData <- function(filename, sheetname = "") {
     mutate(name.fix=str_replace_all(name.fix, "\\s+", " ")) %>% # дубли пробелов
     mutate(name.fix=str_replace_all(name.fix, "^(:: )+|(:: )+$", "")) # NA строк в начале или конце
   
-  dput(t$name.fix)
-  write(t$name.fix, "name.fix", append=FALSE)
+  # dput(cnames$name.fix)
+  # write(cnames$name.fix, "name_fix.txt", append=FALSE)
   
-  df1 <- df0
-  repl.df <- frame_data(
-    ~pattern, ~replacement,
-    "Формующая часть: Угол напорного ящика", "angle_in",
-    "Формующая часть: Разница скорости струи/сетки", "speed_diff_in",
-    "Формующая часть: Открытие щели напорного ящика", "slot_in",
-    "Формующая часть: Давление на напорном ящике", "pressure_in",
-    "Поток: Концентрация при размоле", "concentration_in",
-    "Производительность", "performance_out",
-    "Вес м2", "weight_out",
-    "Артикул", "mark_out"
-  )
-  names(df1) <- stri_replace_all_fixed(names.df$name.fix,
-                                       pattern = repl.df$pattern,
-                                       replacement = repl.df$replacement,
-                                       vectorize_all = FALSE)
-  
-  
-  # Все равно кривые имена, дубли
-  names.df %>% 
+  # проверим кривые имена, дубли
+  cnames %>% 
     group_by(name.fix) %>% 
     filter(n()>1)
   
+  # аналитику необходимо вести по колонкам с английскими именами
+  df1 <- df0
+  repl.df <- frame_data(
+    ~pattern, ~replacement,
+    "Определение проекта(ОИП)", "oip_full",
+    "Дата ввода ОФ", "date",
+    "Стоимость стройки по ПД в базовых ценах:: Общий результат:: * 1 RUB", "pd_cost",
+    "Стоимость стройки по ПД в базовых ценах:: Подрядные работы, в т.ч. материалы:: * 1 RUB", "smp_cost",
+    "Стоимость стройки по ПД в базовых ценах:: Оборудование:: * 1 RUB", "plant_cost"
+  )
+  
+  names(df1) <- stri_replace_all_fixed(cnames$name.fix,
+                                       pattern = repl.df$pattern,
+                                       replacement = repl.df$replacement,
+                                       vectorize_all = FALSE)
   df1 %<>% repair_names(prefix = "repaired_", sep = "")
   
   # выбираем только интересующие колонки
-  df2 <- df1 %>% select(angle_in, speed_diff_in, slot_in, pressure_in, concentration_in,
-                        performance_out, weight_out, mark_out) %>%
-    filter(row_number() > 6) %>% # удаляем весь верхний шлак
-    filter(complete.cases(.)) %>% # удаляем строки, содержащие пустые данные
+  df2 <- df1 %>% select(oip_full, date, pd_cost, smp_cost, plant_cost) %>%
+    #filter(row_number() > 6) %>% # удаляем весь верхний шлак
+    # filter(complete.cases(.)) %>% # удаляем строки, содержащие пустые данные
+    mutate(year=year(dmy(date, quiet=TRUE))) %>% # кривые даты превратились в NA, их и заменим
+    replace_na(list(pd_cost='0', smp_cost='0', plant_cost='0', year=0)) %>%
     distinct() %>% # уберем идентичные строчки
     #http://stackoverflow.com/questions/27027347/mutate-each-summarise-each-in-dplyr-how-do-i-select-certain-columns-and-give
-    mutate_each(funs(as.numeric), -mark_out)
-  
+    #mutate_each(funs(as.numeric), pd_cost, smp_cost, plant_cost)
+  mutate_each(funs(as.numeric), smp_cost)
+
   df2
 }
 
@@ -106,6 +98,16 @@ getOksData <- function(filename, sheetname = "") {
 # }
 
 df <- getOksData(data_filename)
+
+df1 <- df %>%
+  # http://stackoverflow.com/questions/22850026/filtering-row-which-contains-a-certain-string-using-dplyr
+  filter(str_detect(oip_full, '\\d{3}-\\d{7}\\.\\d{4}')) %>% # 022-2000791.0250
+  separate(oip_full, into=c("oip", "sub_oip"), sep="[.]", remove=FALSE)
+
+df_out <- df1 %>%
+  group_by(year) %>%
+  summarise(num=n(), pd=sum(pd_cost), smp=sum(smp_cost), plant=sum(plant_cost))
+
 
 stop()
 # write.table(names.df$name.fix, file = "names.csv", sep = ",", col.names = NA, qmethod = "double")
