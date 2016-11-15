5ea8c7fe24304bea3e27de86ecfd3eccbb022538
iMissile <ishutov@gmail.com>
2016-11-15 12:53:48 +0300

    Сделал первый прототип Графика ввода в эксплуатацию ОКС

--- 55 Kazarin/Kkazarin.R
+++ 55 Kazarin/Kkazarin.R
@@ -12,6 +12,9 @@ library(iterators)
 library(foreach)
 library(doParallel)
 library(zoo)
+library(grid) # для grid.newpage()
+library(gridExtra) # для grid.arrange()
+library(RColorBrewer)
 
 
 data_filename <- "./data/отчет с детализацией по ОКС (056-2000815, 022-2000791, 051-2002476).xlsx"
@@ -102,24 +105,55 @@ getOksData <- function(filename, sheetname = "") {
 
 df <- getOksData(data_filename)
 
-m <- df$date
-
-df_out <- df %>%
+df_gp <- df %>%
   group_by(year) %>%
-  summarise(num=n(), pd=sum(pd_cost), smp=sum(smp_cost), plant=sum(plant_cost)) %>%
+  summarise(num=n(), pd=sum(pd_cost), smp=sum(smp_cost), plant=sum(plant_cost))
+
+df_gp1 <- df_gp %>%
+  select(-num) %>%
   gather(key=type, value=cost, -year) # превратили в long для отрисовки
 
-# plot(df %>% dplyr::select(-mark_out, -month))
+df_gp2 <- df_gp %>%
+  select(year, num)
 
-gp <- ggplot(data = df_out, aes(x=year, y=cost, fill=type)) +
+xlims <- c(min(df_out$year)-1, max(df_out$year)+1)
+
+gp1 <- ggplot(data = df_gp1, aes(x=year, y=cost, fill=type)) +
   theme_bw() +
   # theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
-  geom_bar(stat="identity", position="dodge") #  +
+  scale_fill_brewer(palette="Set1", direction=1, guide=guide_legend()) +
+  geom_bar(stat="identity", position="dodge") +
+  theme(legend.position="top") +
   #geom_point(size = 3, fill = "yellow", shape = 21, na.rm = TRUE) +    # White fill
   #geom_abline(intercept = 0, slope = 1, colour = "red") +
-  # labs(x = "Год", y = "Цена, руб")
+  scale_x_continuous(limits=xlims, breaks=seq(xlims[[1]], xlims[[2]], 1)) +
+  labs(x="Год", y="Сметная стоимость\n по ПД, руб.")
 
-gp
+gp1
+
+gp2 <- ggplot(data = df_gp2, aes(x=year, y=num)) +
+  theme_bw() +
+  geom_point(size=3, fill="yellow", shape=21, na.rm=TRUE) +    # White fill
+  geom_line(colour="red") +
+  scale_x_continuous(limits=xlims, breaks=seq(xlims[[1]], xlims[[2]], 1)) +
+  labs(x="Год", y="Количество ОКС, ед.")
+
+gp2
 
 stop()
+# Table
+gp3 <- tableGrob(head(spread(df_out, year, cost)))
+grid.draw(gp3)
+
+# рисуем графики один под другим с синхронизированными осями
+grid.arrange(gp1, gp2, gp3, ncol = 1) # возвращаем ggplot
+grid.newpage()
+grid.draw(rbind.gtable(ggplotGrob(gp1), ggplotGrob(gp2), size = "max"))
+
+
+# grid.table(df_gp)
+grid.newpage()
+grid.arrange(rbind.gtable(ggplotGrob(gp1), ggplotGrob(gp2), size = "max"), gp3, ncol = 1) # возвращаем ggplot
+
+
 
