611f7994b5624aa7252fe0d6665e9ad4ea7076fc
iMissile <ishutov@gmail.com>
2016-11-18 14:02:36 +0300

    удалось загрузить все отчеты

--- 55 Kazarin/common_funcs.R
+++ 55 Kazarin/common_funcs.R
@@ -1,6 +1,7 @@
 getOksData <- function(filename, sheetname = "") {
   # хак по считыванию типов колонок
   col_types <- readxl:::xlsx_col_types(filename)
+  col_types <- rep("text", length(col_types))
   
   #col_types <- rep("text", length(col_types))
   raw <- read_excel(filename,
