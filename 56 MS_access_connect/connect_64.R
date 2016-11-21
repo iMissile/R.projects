43652a155d6c067b01c3c985b6c8435eb3087394
iMissile <ishutov@gmail.com>
2016-11-21 13:23:52 +0300

    Удалось подключить к Access Database под Win64\R64

--- 56 MS_access_connect/connect_64.R
+++ 56 MS_access_connect/connect_64.R
@@ -2,7 +2,19 @@ library("RODBC") #load package
 
 # эксперимент №1: https://gist.github.com/dainiuxt/23ce714e0deedc8da9a0
 
-db<-file.path("./data/Access2007PureSQL.accdb") #connect database.
+Sys.setenv(LANG = "en")
+db <- file.path("./data/Access2007PureSQL.accdb") #connect database.
+# normalizePath(db, winslash="/")
+# db <- file.path("D:/iwork.GH/R.projects/56 MS_access_connect/data")
+# db <- file.path("Access2007PureSQL.accdb") #connect database.
+
 #Note the UNIX style slash (/). "\" is "escape character" so all "\"you should replace either with "/" or "\\"
-channel<-odbcConnectAccess2007(db) #internal RODBC function
-dataSetName<-sqlFetch(channel,"TableName") #read particular table from Access database file.
+# channel <- odbcConnectAccess2007(db) #internal RODBC function
+# outputFile = paste(normalizePath(dirname(inputFile)),"\\", "my_file.ext", sep = "")
+channel <- odbcConnectAccess(db) #internal RODBC function
+# Error in odbcConnectAccess(db): odbcConnectAccess is only usable with 32-bit Windows
+channel <- odbcConnectAccess2007(db) #internal RODBC function
+
+table_list <- sqlTables(channel) # запросим список таблиц
+dataSetName <- sqlFetch(channel, "Products") #read particular table from Access database file.
+close(channel) #do not forget this, otherwise you lock access database from editing.
