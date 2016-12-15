14bf6353d5bba962fe1f62c8882e8cdaa4076f4d
iMissile <ishutov@gmail.com>
2016-12-15 15:11:50 +0300

    Ура, проблема с ручной расстановкой вершин решена!

--- 62 a_la_weathermap/graph_examples.R
+++ 62 a_la_weathermap/graph_examples.R
@@ -5,7 +5,7 @@ library(sna)
 library(intergraph) # http://mbojan.github.io/intergraph/
 library(igraph)
 # library(magrittr)
-library(ggnetwork) # https://cran.r-project.org/web/packages/ggnetwork/vignettes/ggnetwork.htmlpacman::p_load("ggnetwork", "ggnet", "geomnet")
+library(ggnetwork) # https://cran.r-project.org/web/packages/ggnetwork/vignettes/ggnetwork.html
 
 # Example 1 -----------------------
 # https://cran.r-project.org/web/packages/ggnetwork/vignettes/ggnetwork.html#edge-weights
@@ -20,5 +20,25 @@ g <- asIgraph(n)
 # http://igraph.org/r/doc/graph_from_literal.html
 # In directed graphs, edges will be created only if the edge operator includes a arrow head (‘+’) 
 g <- graph_from_literal(A-+B-+C, D-+A, C+-E-+D, E-+B)
+V(g) # Vertex sequence
+E(g) # Edge sequence
 net <- asNetwork(g)
-plot(g, layout=layout_on_grid(g))
+g <- g %>% add_layout_(on_grid())
+# plot(g, layout=layout_on_grid(g))
+plot(g)
+plot(g, layout=layout_as_star(g))
+
+# We now add a categorical edge attribute called "type", which is set to either "x", "y" or "z", 
+# and a continuous vertex attribute called  "day", which is set to either 1, 2 or 3.
+e <- network.edgecount(net)
+set.edge.attribute(net, "type", sample(letters[24:26], e, replace=TRUE))
+set.edge.attribute(net, "day", sample(1:3, e, replace = TRUE))
+m <- ggnetwork(g)
+
+n <- ggnetwork(net, layout = "target")
+ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
+  geom_edges(aes(linetype=type, color=type, lwd=type)) +
+  geom_nodelabel(aes(label=vertex.names),
+                 fontface="bold") +
+  theme_blank()
+
