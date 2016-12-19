f57f04b81dff3ecd9d63c777381780a4a6b402ce
iMissile <ishutov@gmail.com>
2016-12-19 18:29:44 +0300

    Добавил логирование, изображение выводится

--- 62 a_la_weathermap/dynamic_net_map/app_network.R
+++ 62 a_la_weathermap/dynamic_net_map/app_network.R
@@ -8,9 +8,15 @@ library(ggplot2)
 # library(ggpmisc)
 library(ggnetwork)
 library(shiny)
+library(futile.logger)
+
 # http://shiny.rstudio.com/gallery/plot-interaction-basic.html
 # https://shiny.rstudio.com/articles/debugging.html
 # https://blog.rstudio.org/2015/06/16/shiny-0-12-interactive-plots-with-ggplot2/
+
+flog.appender(appender.file('network.log'))
+flog.threshold(TRACE)
+flog.info("Network dashboard started")
 
 
 options(shiny.error=browser)
@@ -49,7 +55,11 @@ ui <- fluidPage(
            ),
     column(width = 5,
            # А здесь мы нарисуем отрендеренный вручную график
-           imageOutput("image_render", height = 350
+           imageOutput("image", height = 350,
+                       click = "plot_click",
+                       dblclick = "plot_dblclick",
+                       hover = "plot_hover",
+                       brush = "plot_brush"
            )
     )
     
@@ -57,23 +67,15 @@ ui <- fluidPage(
     ),
 
   fluidRow(
-    column(width = 3,
-           verbatimTextOutput("click_info")
-    ),
-    column(width = 3,
-           verbatimTextOutput("dblclick_info")
-    ),
-    column(width = 3,
-           verbatimTextOutput("hover_info")
-    ),
-    column(width = 3,
-           verbatimTextOutput("brush_info")
-    )
+    column(width = 3, verbatimTextOutput("click_info")),
+    column(width = 3, verbatimTextOutput("dblclick_info")),
+    column(width = 3, verbatimTextOutput("hover_info")),
+    column(width = 3, verbatimTextOutput("brush_info"))
   )
     )
 
 # Define server logic required to draw a network
-server <- function(input, output) {
+server <- function(input, output, session) {
   # определяем сетевой объект на уровне сессии пользователя
   g <- graph_from_literal(A-+B-+C, D-+A, C+-E-+D, E-+B)
   set.seed(123)
@@ -111,6 +113,58 @@ server <- function(input, output) {
     }
   })
 
+  output$image <- renderImage({
+      gp <- ggplot(net, aes(x=x, y=y, xend=xend, yend=yend)) +
+        # geom_edges(aes(linetype=type, color=type, lwd=type)) +
+        geom_edges(aes(linetype=type), color="grey75", lwd=1.2)+ #  , curvature = 0.1) +
+        geom_nodes(color="gold", size=8) +
+        # geom_nodelabel(aes(label=vertex.names), fontface="bold") +
+        # geom_nodelabel_repel(aes(color=ip_addr, label=vertex.names), fontface = "bold", box.padding=unit(2, "lines")) +
+        geom_nodelabel_repel(aes(label=ip_addr), fontface = "bold", box.padding=unit(2, "lines"), 
+                             segment.colour="red", segment.size=1) +
+        geom_edgetext_repel(aes(label=volume), color="white", fill="grey25",
+                            box.padding = unit(1, "lines")) +
+        theme_blank() +
+        theme(axis.text = element_blank(),
+              axis.title = element_blank(),
+              panel.background = element_rect(fill = "grey25"),
+              panel.grid = element_blank())
+      
+      flog.info("ggplot for Image built")
+      
+      # сохраним в файл, разбираемся с антиалайзингом
+      # http://blog.revolutionanalytics.com/2009/01/10-tips-for-making-your-r-graphics-look-their-best.html
+      # http://gforge.se/2013/02/exporting-nice-plots-in-r/ (см. отдельно UPDATE и "lines and text anti-aliased - not fills/polygons")
+      outfile <- "render_w_cairo.png"
+      # outfile <- tempfile(fileext='.png')
+      flog.info(paste0("rendered file:", outfile))
+      
+      # Read plot2's width and height. These are reactive values, so this
+      # expression will re-run whenever these values change.
+      width  <- session$clientData$output_image_width
+      height <- session$clientData$output_image_height
+      flog.info(paste0("render size (H x W): ", height, " x ", width))
+      
+      
+      png(filename=outfile, type="cairo", #pointsize=24, 
+          units="cm", height=10, width=20, res=150, pointsize=8, antialias="default")
+      print(gp)
+      dev.off()
+      flog.info("Image rendered")
+      
+      
+      # Return a list containing information about the image
+      list(
+        src = outfile,
+        #contentType = "image/png",
+        #width = width,
+        #height = height,
+        alt = "This is alternate text"
+      )      
+      
+  }, deleteFile=FALSE)
+  
+  
     
   output$click_info <- renderPrint({
     cat("input$plot_click:\n")
