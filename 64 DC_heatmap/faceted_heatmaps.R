db1573a4f0f17a8e8945f42fdc5a37ecf660b5b5
iMissile <ishutov@gmail.com>
2017-02-10 18:30:19 +0300

    После всех экспериментов с heatmap все прибрал в коде

--- 64 DC_heatmap/faceted_heatmaps.R
+++ 64 DC_heatmap/faceted_heatmaps.R
@@ -11,64 +11,33 @@ library(extrafont)   # http://blog.revolutionanalytics.com/2012/09/how-to-use-yo
 
 # см. https://rpubs.com/omicsdata/faceted_heatmap
 
-attacks <- read_csv("./data/eventlog.csv", col_types="ccc", progress=interactive()) %>%
-  slice(1:20)
+attacks_raw <- read_csv("./data/eventlog.csv", col_types="ccc", progress=interactive()) %>%
+  slice(1:20000)
   #sample_n(20, replace=FALSE)
 
-
-if(TRUE){
-# рабочее решение в старой парадигме отсюда: https://rud.is/projects/facetedheatmaps.html
-# но это работает очень быстро, ~ 100 раз быстрее
-parseDayParts <- function(cc, ts, tz) {
-  real_times <- ymd_hms(ts, tz=tz[1], quiet=TRUE)
-  data_frame(timestamp=ts,
-             source_country=cc, 
-             wkday=weekdays(as.Date(real_times, tz=tz[1])),
-             hour=format(real_times, "%H", tz=tz[1]))
+# ================== решение из статьи, переписанной мной на nest\map идеологию
+parseDFDayParts <- function(df, tz) {
+  real_times <- ymd_hms(df$timestamp, tz=tz, quiet=TRUE)
+  tibble(wkday=weekdays(as.Date(real_times, tz=tz)),
+         hour=as.numeric(format(real_times, "%H", tz=tz)))
 }
 
-attacks2 <- attacks %>%
-group_by(tz) %>%
-  do(parseDayParts(.$source_country, .$timestamp, .$tz)) %>% 
-  ungroup()
-}
-
-attacks4 <- attacks %>%
+attacks <- attacks_raw %>%
   group_by(tz) %>%
   nest()
 
-## а так заработало!!!
-parseDFDayParts <- function(df, tz) {
-  browser()
-  real_times <- ymd_hms(df$timestamp, tz=tz, quiet=TRUE)
-  tibble(wkday=weekdays(as.Date(real_times, tz=tz)),
-         hour=format(real_times, "%H", tz=tz))
-}
-
-
-attacks5 <- attacks4 %>%
+attacks <- attacks %>%
   mutate(res=map2(data, tz, parseDFDayParts)) %>%
-  unnest()
-
-
-
-# рабочее решение отсюда: https://github.com/rstudio/rstudio-conf/tree/master/2017/List_Columns-Jenny_Bryan
-attacks <- attacks %>% 
-  # mutate(rt=map(.$timestamp, ~ ymd_hms(.x, quiet=FALSE))) %>% # с этим подходом ~ в 8 раз медленнее
-  mutate(rt=ymd_hms(timestamp, quiet=FALSE)) %>%
-  mutate(hour=as.numeric(map2(.$rt, .$tz, ~ format(.x, "%H", tz=.y)))) %>%
-  # mutate(hour2=lubridate::hour(rt)) %>%
-  mutate(wkday_text=map2(.$rt, .$tz, ~ weekdays(as.Date(.x, tz=.y)))) %>%
-  unnest(rt, hour, wkday_text) %>%
-# превратим wkday в фактор принудительно с понедельника
-  mutate(wkday=factor(wkday_text, levels=weekdays(dmy("13.02.2017")+0:6)))
+  unnest() %>%
+  # превратим wkday в фактор принудительно с понедельника
+  mutate(wkday=factor(wkday, levels=weekdays(dmy("13.02.2017")+0:6)))
 
 
 wkdays <- count(attacks, wkday, hour)
 wkdays
 
 # разбираемс€ с кастомными шрифтами
-font_import(paths = NULL, recursive = TRUE, prompt = TRUE, pattern = NULL)
+# font_import(paths = NULL, recursive = TRUE, prompt = TRUE, pattern = NULL)
 # In doing this your specifying explicitly the Windows Font mapping.
 # windowsFonts(Times=windowsFont("TT Times New Roman"))
 windowsFonts(Verdana="TT Verdana")
@@ -77,7 +46,7 @@ windowsFonts(Verdana="TT Verdana")
 gg = ggplot(wkdays, aes(x=hour, y=wkday, fill=n))
 gg = gg + geom_tile(color="white", size=0.1)
 gg = gg + scale_fill_viridis(option="B", name="# Events", label=comma) # https://cran.r-project.org/web/packages/viridis/vignettes/intro-to-viridis.html
-gg = gg + scale_fill_distiller(palette="RdYlGn", name="# Events", label=comma) # http://docs.ggplot2.org/current/scale_brewer.html
+#gg = gg + scale_fill_distiller(palette="RdYlGn", name="# Events", label=comma) # http://docs.ggplot2.org/current/scale_brewer.html
 #gg = gg + coord_equal()
 gg = gg + coord_fixed(ratio = 1)
 gg = gg + labs(x=NULL, y=NULL, title="Events per weekday & hour of day")
@@ -85,6 +54,40 @@ gg = gg + theme_tufte(base_family="Verdana")
 gg = gg + theme(plot.title=element_text(hjust=0))
 
 stop()
+
+# ================== решение из статьи
+if(TRUE){
+  # рабочее решение в старой парадигме отсюда: https://rud.is/projects/facetedheatmaps.html
+  # но это работает очень быстро, ~ 100 раз быстрее
+  parseDayParts <- function(cc, ts, tz) {
+    real_times <- ymd_hms(ts, tz=tz[1], quiet=TRUE)
+    data_frame(timestamp=ts,
+               source_country=cc, 
+               wkday=weekdays(as.Date(real_times, tz=tz[1])),
+               hour=format(real_times, "%H", tz=tz[1]))
+  }
+  
+  attacks2 <- attacks_raw %>%
+    group_by(tz) %>%
+    do(parseDayParts(.$source_country, .$timestamp, .$tz)) %>% 
+    ungroup()
+}
+
+# ==================неэффективное применение map!!!!¬се из-за tz и поштучной обработки
+if(FALSE){
+  # рабочее решение отсюда: https://github.com/rstudio/rstudio-conf/tree/master/2017/List_Columns-Jenny_Bryan
+  attacks <- attacks %>% 
+    # mutate(rt=map(.$timestamp, ~ ymd_hms(.x, quiet=FALSE))) %>% # с этим подходом ~ в 8 раз медленнее
+    mutate(rt=ymd_hms(timestamp, quiet=FALSE)) %>%
+    mutate(hour=as.numeric(map2(.$rt, .$tz, ~ format(.x, "%H", tz=.y)))) %>%
+    # mutate(hour2=lubridate::hour(rt)) %>%
+    mutate(wkday_text=map2(.$rt, .$tz, ~ weekdays(as.Date(.x, tz=.y)))) %>%
+    unnest(rt, hour, wkday_text) %>%
+    # превратим wkday в фактор принудительно с понедельника
+    mutate(wkday=factor(wkday_text, levels=weekdays(dmy("13.02.2017")+0:6)))
+}
+
+
 
 # ================ ничего так и не работает нормально с датами....
 
