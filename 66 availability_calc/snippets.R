898f544fc3e12cdd783bfa9f26cf0279c6d24d7c
iMissile <ishutov@gmail.com>
2017-04-06 11:13:09 +0300

    Ура, честно заработало для 1288 в математике!

--- 66 availability_calc/snippets.R
+++ 66 availability_calc/snippets.R
@@ -4,7 +4,7 @@ library(purrr)
 library(ggplot2)
 
 
-PSystem <- function(n=0, m=0, lambda, t){
+pSystem <- function(n=0, m=0, lambda, t){
   # пока не векторизуем
   # t - время для которого надо провести расчет
   
@@ -15,6 +15,31 @@ PSystem <- function(n=0, m=0, lambda, t){
   sum(df$elem) * exp(-(n-m)*lambda*t)
 }
 
+
+lSystem <- function(n=0, m=0, lambda, t){
+  # пока не векторизуем
+  # t - время для которого надо провести расчет
+  
+  df <- tibble(k=seq(0, m)) %>%
+    mutate(elem=((n-m)*lambda*t)^k/factorial(k))
+
+  res <- (n-m)*lambda*((n-m)*lambda*t)^m/factorial(m)*sum(df$elem)
+}
+
+tSystem <- function(n=0, m=0, lambda, t){
+  # пока не векторизуем
+  # t - фиктивное время
+
+  res <- (m+1)/(n-m)*(1/lambda)
+}
+
 time <- seq(0, 100, by=5)
-p <- purrr::map(time, ~ PSystem(2, 1, 0.05, .x))
-qplot(time, unlist(p))
+
+ps <- purrr::map(time, ~ pSystem(3, 2, 0.05, .x))
+qplot(time, unlist(ps))
+
+ls <- purrr::map(time, ~ lSystem(3, 2, 0.05, .x))
+qplot(time, unlist(ls))
+
+ts <- purrr::map(time, ~ tSystem(3, 2, 0.05, .x))
+qplot(time, unlist(ts))
