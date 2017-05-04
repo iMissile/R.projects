3c32f2b67e50b9bd1a0f2e42bc297d3bc17e8943
iMissile <ishutov@gmail.com>
2017-05-04 15:13:46 +0300

    Разбираем проблемы со сборкой вложенных списков в data.frame. rlist, respahe и др

--- 73 elastic_test/1-st play.R
+++ 73 elastic_test/1-st play.R
@@ -1,4 +1,12 @@
-library("elastic")
+library(tydyverse)
+library(tibble)
+library(purrr)
+library(magrittr)
+library(data.tree)
+library(rlist)
+library(reshape2)
+library(pipeR)
+library(elastic)
 
 
 connect(es_host="89.223.29.108", es_port = 9200)
@@ -57,13 +65,53 @@ body <- '{
 }'
 
 # надо оптимизировать размер size исходя из совокупного времени исполнения запросов + склейки
-req_size <- 2000
+req_size <- 2
 res <- Search(index="packetbeat-*", body=body, scroll="1m", size=req_size)
-res$hits$total
-seq(from=1, by=1, length.out=res$hits$total/req_size)
+m <- res$hits$hits
 
-# посмотрим длину скрола
+jl <- as.Node(m)
+list.names(m)
+rl <- list.select(m, type=`_type`, timestamp=`_source`$`@timestamp`) # так достается с помощью rlist
+rl <- list.select(m, start_time=`_source`$`start_time`, 
+                  src_ip=`_source`$source$ip,
+                  src_port=`_source`$source$port,
+                  bytes_sent=`_source`$source$stats$net_bytes_total) # так достается с помощью rlist
+
+# df <- list.stack(rl) # выдает ошибку, если поле NULL: Error in data.table::rbindlist(.data, ...) : Column 3 of item 1 is length 0, inconsistent 
+
+rl2 <- map(rl, function(x){str(x); cat("--"); map(x, function(z) ifelse(is.null(z), NA, z))})
+
+dplyr::bind_rows(rl) # когда нет полей NULL, то все срабатывает
+dplyr::bind_rows(rl2) # когда нет полей NULL, то все срабатывает
+# melt(rl) # можно и так + gather
+
+map_val_list <- list(c("start_time", "start_time"),
+                     c("src_port", "source$port"))
+
+# см. фукнцию get_tsdf_from_SW_json
+df <- map(m, 
+          function(x){
+            str(x)
+            tibble(start_time=x$`_source`$start_time,
+                   src_port=x$`_source`$source$port)
+          }
+          )
+
+# надо пробежаться по всему списку, достать нужные параметры и замапировать их на поля data.frame) 
+
+# взглянем на длину данных и длину скрола
+res$hits$total
 length(scroll(scroll_id = res$`_scroll_id`)$hits$hits)
+req_list <- seq(from=1, by=1, length.out=res$hits$total/req_size)
+
+
+processScroll <- function(n){
+  cat("iteration", n, "\n")
+}
+  
+# склеиваем все запросы
+df <- req_list %>%
+  purrr::map_df(processScroll, .id = NULL)
 
 # ========
 Search(index="packetbeat-*", body=body)$hits$total
