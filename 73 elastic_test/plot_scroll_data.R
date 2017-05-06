206c35651d6208c0bfa12d22d7f035861f9c594d
iMissile <ishutov@gmail.com>
2017-05-06 22:00:09 +0300

    Добавил вычисление агрегата и график

--- 73 elastic_test/plot_scroll_data.R
+++ 73 elastic_test/plot_scroll_data.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(scales)
 library(lubridate)
 library(parsedate)
 library(fasttime)
@@ -11,6 +12,7 @@ library(magrittr)
 # library(reshape2)
 # library(pipeR)
 # library(jsonlite)
+library(hrbrthemes)
 library(tidyjson)
 library(profvis)
 library(elastic)
@@ -32,7 +34,7 @@ body <- '{
 {"match": { "source.ip": "10.0.0.232"}} 
 ],
 "filter":  [
-{ "range": { "start_time": { "gte": "now-8h", "lte": "now" }}}
+{ "range": { "start_time": { "gte": "now-36h", "lte": "now" }}}
 ] 
 }
 }
@@ -60,4 +62,29 @@ df <- req_list %>%
 # преобразуем типы
 df2 <- df %>%
   mutate(timestamp=fastPOSIXct(start_time)) %>% # время указано в ISO 8601
-  mutate(timegroup=hgroup.enum(timestamp, min_bin=5))
+  mutate(timegroup=hgroup.enum(timestamp, min_bin=5)) %>%
+  group_by(timegroup) %>%
+  summarise(volume_Mb=sum(bytes_total)/1024/1024) %>%
+  ungroup()
+
+# Может потребоваться добавление пропущенных временных точек
+# tdf<- tibble(timegroup=seq(min(df1$timegroup), max(df1$timegroup) + days(f_depth), by="1 day"))
+
+# разные способы объединения
+# merge(df2, tdf, by="timegroup", all.x=TRUE, all.y=TRUE)
+# res <- full_join(df2, tdf, by="timegroup")
+
+gp <- ggplot(df2, aes(timegroup, volume_Mb)) + 
+  geom_point(alpha=0.85, shape=1, size=3) +
+  geom_line(alpha=0.85, lwd=1) +
+  # theme_ipsum_rc(base_family="robotoC", base_size=16, axis_title_size=14) +
+  scale_x_datetime(breaks=date_breaks("1 hour")
+                   #minor_breaks = date_breaks("6 hours"),
+                   ) +
+  theme_ipsum_rc(base_size=16, axis_title_size=14) +
+  theme(axis.text.x = element_text(angle=90)) +
+  xlab("Дата, время") +
+  ylab("Суммарный объем данных, Mb") +
+  ggtitle("Динамика трафика", subtitle="Суммарный объем в Мегабайтах")
+
+gp
