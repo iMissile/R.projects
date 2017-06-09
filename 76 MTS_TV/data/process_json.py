6cf56b409932f8816356458576c8d7d0c9dbf652
iMissile <ishutov@gmail.com>
2017-06-09 21:30:45 +0300

    Учимся импортировать тяжелый json

--- 76 MTS_TV/data/process_json.py
+++ 76 MTS_TV/data/process_json.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python
 # -*- coding: cp1251 -*-
 import os
+import time
 
 def main():
+    
+    src_name = "stat.json"
+    # src_name = "head.json"
+    
 
     print(os.path.basename(os.getcwd()))
-    fout = open("stat_fix.json", 'w')
-
-    num_lines = sum(1 for line in open('stat.json')) # посчитали общее число строк в файле
+    print(os.getcwd())
+    fin = open(src_name, 'r')
+    num_lines = 0
+    # num_lines = sum(1 for line in fin) # посчитали общее число строк в файле
     print(num_lines)
-    
+    fin.close()
+
+    start = time.time()    
     try:
-        with open("stat.json", 'r') as fin:
-            i = 0
-            for line in fin:
-                # print(value='', sep=' ', end='\n', file=sys.stdout, flush=False)
-                # print(line)
-                i = i + 1
+        fin = open(src_name, 'r')
+        fout = open("stat_fix.json", 'w')
+        i = 0
+        
+        fout.write('[\n')
+        for line in fin:
+            # print(value='', sep=' ', end='\n', file=sys.stdout, flush=False)
+            # print(line)
+            i = i + 1
+            # print(i)
+            fout.write(line.replace('\n', ',') + '\n')
+            
+            if i % 10000 == 0: 
                 print(i)
-                if i % 10 == 0: 
-                    print(i)
     finally:
+        fout.write(']')        
         fin.close()
         fout.close()
-        
+    
+    end = time.time()   
+    print(end - start, "s")
 
 
 if __name__ == '__main__':
--- 76 MTS_TV/import_data.R
+++ 76 MTS_TV/import_data.R
@@ -7,7 +7,7 @@ library(magrittr)
 #library(reshape2)
 #library(pipeR)
 library(jsonlite)
-library(tidyjson)
+#library(tidyjson)
 #library(elastic)
 #library(parsedate)
 #library(fasttime)
@@ -17,12 +17,22 @@ library(tictoc)
 
 # https://jsonformatter.curiousconcept.com/
 
+if (FALSE){
 fname <- "./data/head.json"
+fname <- "./data/stat_fix.json"
+# fname <- "./data/stat_small.json"
+
 
 tic()
 # заменим концы строк на `,`` и добавим шапочку и окончание для формирования семантически правильного json
 # последнюю ',' надо удалить, может такое встретиться (перевод строки)
 # tmp <- paste0('{"res":[', gsub("\\n", ",\n", wrecs, perl = TRUE), ']}')
+tvstream <- stream_in(file("./data/stat_small.json"), pagesize = 10000)
 
-df <- jsonlite::fromJSON(fname, simplifyDataFrame=TRUE)
+#df <- jsonlite::fromJSON(fname, simplifyDataFrame=TRUE)
 toc()
+
+system.time(saveRDS(tvstream, "tvstream2.rds", ascii=FALSE, compress="gzip"))
+}
+
+system.time(df <- readRDS("./data/tvstream2.rds"))
