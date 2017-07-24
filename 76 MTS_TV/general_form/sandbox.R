4992b7d818f5525363c3980424b63bbb8d88214b
iMissile <ishutov@gmail.com>
2017-07-24 14:30:22 +0300

    пытаюсь вернуть порядок

--- 76 MTS_TV/general_form/sandbox.R
+++ 76 MTS_TV/general_form/sandbox.R
@@ -10,12 +10,11 @@ library(futile.logger)
 library(anytime)
 library(tictoc)
 library(digest)
-#library(rJava)
-#library(ReporteRs)
+library(rJava)
+library(ReporteRs)
 library(officer)
 library(extrafont)
 library(hrbrthemes)
-library(Unicode)
 
 
 #eval(parse("funcs.R", encoding="UTF-8"))
@@ -134,26 +133,7 @@ if(TRUE){
     ggtitle("Статистика телесмотрения", subtitle="Топ 9 регионов") +
     coord_flip()  
 
-  out_df <- as_tibble(raw_df[1:80, ]) %>%
-    # select(timestamp, 'Яанал'=channelId)
-    select("Канал"=channelId)
-    #select(timestamp, stri_enc_toutf8('Канал')=channelId, 'Сегмент'=segment, 
-    #       'Регион'=region, "Ну просто очень-очень длинный заголовок"=programId)
-
-  # browser()
-  # имена колонок надо превратить в unicode
-  # nm <- names(out_df) %>% stri_conv(from="windws-1251", to="UTF-8", to_raw=FALSE)
-  nm <- names(out_df)
-  #stri_enc_mark(nm)
-  #nm <- names(out_df) %>% 
-  #  stri_conv(from="windows-1251", to="UTF-8", to_raw=FALSE) %>% 
-  #  map(`Encoding<-`, "UTF-8")
-  
-  stri_enc_mark(nm)
-  
-  names(out_df) <- nm
-
-  # browser()
+  out_df <- raw_df[1:80, ] %>% select(timestamp, region, programId, segment)
   
   # создаем файл ------------------------------------------
   doc <- read_docx() %>% # read_docx(path="./TV_report_template.docx") %>%
@@ -164,7 +144,6 @@ if(TRUE){
     print(target = "word_report_officer.docx")
 }
 
-stop()
 
 # ====================================================================
 # Отчет №1: "Рейтинг каналов" --------------------
