da99dd28a4a0fc4205c8126dda0907a87a57983d
iMissile <ishutov@gmail.com>
2017-08-04 16:09:59 +0300

    Начало работы над формой для гибких селектов

---
+++ 76 MTS_TV/00_general/app.R
@@ -0,0 +1,374 @@
+# Single-file Shiny apps (http://shiny.rstudio.com/articles/single-file.html)
+# Обязательно в кодировке UTF-8
+rm(list=ls()) # очистим все переменные
+gc()
+
+library(tidyverse)
+library(lubridate)
+library(scales)
+library(forcats)
+library(readxl)
+library(magrittr)
+library(stringi)
+library(stringr)
+library(futile.logger)
+library(jsonlite)
+library(Cairo)
+library(RColorBrewer)
+library(extrafont)
+library(hrbrthemes)
+library(DBI)
+library(RPostgreSQL)
+library(config)
+library(shiny)
+library(shinyjqui)
+library(shinythemes) # https://rstudio.github.io/shinythemes/
+library(shinyBS)
+library(shinyjs)
+library(shinycssloaders)
+library(config)
+library(anytime)
+library(tictoc)
+library(digest)
+library(officer)
+
+options(shiny.usecairo=TRUE)
+options(shiny.reactlog=TRUE)
+options(spinner.type=4)
+
+source("clickhouse.R")
+eval(parse("funcs.R", encoding="UTF-8"))
+
+# очистим все warnings():
+assign("last.warning", NULL, envir = baseenv())
+
+# ================================================================
+ui <- 
+  navbarPage("DVT IoT",
+  # title=HTML('<div><a href="http://devoteam.com/"><img src="./img/devoteam_176px.png" width="80%"></a></div>'),
+  title = "Статистика телесмотрения",
+  tabPanel("Рейтинг каналов", value="general_panel"),
+  tabPanel("About", value="about"),
+  # windowTitle="CC4L",
+  # collapsible=TRUE,
+  id="tsp",
+  # theme=shinytheme("flatly"),
+  theme=shinytheme("yeti"),
+  # shinythemes::themeSelector(),
+  # includeCSS("styles.css"),
+
+  # http://stackoverflow.com/questions/25387844/right-align-elements-in-shiny-mainpanel/25390164
+  tags$head(tags$style(".rightAlign{float:right;}")), 
+
+  # titlePanel("Статистика телесмотрения"),
+  
+  conditionalPanel(
+    # general panel -----------------------
+    condition = "input.tsp == 'general_panel'",
+    fluidRow(
+      tags$style(type='text/css', '#cweather_text {white-space:pre;}')
+      # tags$style(type='text/css', 'div {background-color: #000с00;}'), 
+      
+      #column(6, h2("Типовая форма"), h3(textOutput("cweather_text", inline=TRUE))),
+      #column(6, h2("Заполнитель"))
+      ),
+    fluidRow(
+      column(2, dateRangeInput("in_date_range",
+                               label="Диапазон дат",
+                               start=Sys.Date()-1, end=Sys.Date(),
+                               # на время отладки
+                               # start="2017-06-28", end="2017-06-30",
+                               # min = Sys.Date() - 10, 
+                               max = Sys.Date(),
+                               separator = " - ", format = "dd/mm/yy",
+                               startview = "month", language = 'ru', weekstart=1)
+      ), 
+      column(1, selectInput("history_depth", "История", 
+                            choices = c("1 месяц"=30, "2 недели"=14,
+                                        "1 неделя"=7, "3 дня"=3, "1 день"=1), selected=1)),
+      #column(1, selectInput("min_watch_time", "Мин. время",
+      #                      choices = c("5 сек"=5, "10 сек"=10, 
+      #                                  "20 сек"=20, "30 сек"=30), selected = 10)),
+      #column(1, selectInput("max_watch_time", "Макс. время",
+      #                      choices = c("1 час"=1, "2 часа"=2, 
+      #                                  "3 часа"=3, "4 часа"=4), selected = 2)),
+      column(6, uiOutput("choose_region")),
+      column(2, selectInput("segment_filter", "Сегмент",
+                            choices = c("Все"="all",
+                                        "DVB-C"="DVB-C", 
+                                        "IPTV"="IPTV", 
+                                        "DVB-S"="DVB-S"), selected="all"))
+    ),
+    fluidRow(
+      column(10, actionButton("set_test_dates_btn", "Вкл. демо дату", class = 'rightAlign')),
+      column(2, actionButton("process_btn", "Применить", class = 'rightAlign'))
+      ),
+
+    #tags$style(type='text/css', "#in_date_range { position: absolute; top: 50%; transform: translateY(-80%); }"),
+    tabsetPanel(
+      id = "panel_id",
+      selected="table_tab",
+      tabPanel("Таблица", value = "table_tab",
+               fluidRow(
+                 p(),
+                 column(12, div(withSpinner(DT::dataTableOutput('stat_table'))), style="font-size: 90%")
+               ),
+               p(),
+               fluidRow(
+                 column(8, {}),
+                 column(2, downloadButton("csv_download_btn", label="Экспорт (Excel)", class = 'rightAlign')),
+                 column(2, downloadButton("word_download_btn", label="Экспорт (Word)", class = 'rightAlign'))
+               )
+      ),
+      tabPanel("График", value = "graph_tab",
+               fluidRow(
+                 p(),
+                 jqui_sortabled(
+                   div(id='top10_plots',
+                 column(6, div(withSpinner(plotOutput('top10_duration_plot', height="500px")))),
+                 column(6, div(withSpinner(plotOutput('top10_stb_plot', height="500px"))))
+                       )))
+               )
+      )
+    #,
+    #fluidRow(
+    #  column(6, textOutput('info_text'))
+    #)
+    
+  ),
+  shinyjs::useShinyjs()  # Include shinyjs
+)
+
+
+# ================================================================
+server <- function(input, output, session) {
+  # статические переменные ------------------------------------------------
+  log_name <- "app.log"
+  
+  flog.appender(appender.tee(log_name))
+  flog.threshold(TRACE)
+  flog.info("App started")
+
+  # создание параметров оформления для различных видов графиков (screen\publish) ------
+  font_sizes <- list(
+    "screen"=list(base_size=20, axis_title_size=18, subtitle_size=15),
+    "word_A4"=list(base_size=14, axis_title_size=12, subtitle_size=11)
+  )
+  
+  # создаем коннект к инстансу CH -----------
+  if (Sys.info()["sysname"] == "Linux") {
+    # CTI стенд
+    con <- dbConnect(clickhouse(), host="172.16.33.74", port=8123L, user="default", password="")
+  }else{
+    # MT стенд
+    con <- dbConnect(clickhouse(), host="10.0.0.44", port=8123L, user="default", password="")
+  }      
+  
+  # подгрузим таблицу преобразования транслита в русские названия городов -------
+  cities_df <- {
+    flog.info("Loading cities translit table")
+    # подгрузим ограниченный список городов
+    city_subset <- read_csv("region.csv")
+    
+    df <- req(dbGetQuery(con, "SELECT * FROM regnames")  %>%
+                mutate_if(is.character, `Encoding<-`, "UTF-8") %>%
+                filter(translit %in% pull(city_subset)))
+    flog.info(paste0("Cities translit table loaded ", nrow(df), " rows"))
+    # dbDisconnect(con)
+    df
+  }
+  
+  
+  
+  # подгрузим таблицу преобразования идентификатора канала в русское название ----
+  progs_df <- jsonlite::fromJSON("./channels.json", simplifyDataFrame=TRUE) %>% 
+    select(channelId, channelName=name)
+  
+  # реактивные переменные -------------------
+  raw_df <- reactive({
+    input$process_btn # обновлять будем вручную
+    isolate({
+      
+      # regions <- c("Moskva", "Barnaul")
+      regions <- input$region_filter
+      # browser()
+    
+      # r <- buildReq(begin=today(), end=today()+days(1), regions)
+      flog.info(paste0("Applied time filter [", input$in_date_range[1], "; ", input$in_date_range[2], "]"))
+      flog.info(paste0("Applied region filter [", regions, "]"))
+      r <- buildReq(begin=input$in_date_range[1], end=input$in_date_range[2], 
+                    regions=regions, segment=input$segment_filter)
+      flog.info(paste0("DB request: ", r))
+    })
+    
+    # browser()
+
+    tic()
+    temp_df <- dbGetQuery(con, r) %>%
+      as_tibble()
+
+    flog.info(paste0("Query: ", capture.output(toc())))
+    flog.info(paste0("Table: ", capture.output(head(temp_df, 2))))
+    # system.time(df <- readRDS("./data/tvstream4.rds"))
+    flog.info(paste0("Loaded ", nrow(temp_df), " rows"))
+    
+    # !!! исправляем непонятно чей косяк: если вложенный select дает 0 строк, то его имя транслируется как NULL
+    names(temp_df)[[3]] <- "total_unique_stb"
+    
+    # browser()
+    df <- temp_df %>%
+      # время смотрения, мин
+      mutate(channel_duration=round(as.numeric(channel_duration), 0)) %>%
+      # 6. Среднее время просмотра, мин
+      mutate(mean_duration=round(channel_duration/watch_events, 0)) %>%
+      # 3. % уникальных приставок
+      mutate(stb_ratio=round(unique_stb/total_unique_stb, 3)) %>%
+      # 5. % времени просмотра
+      mutate(watch_ratio=round(channel_duration/sum(channel_duration), 5)) %>%
+      # 7. Среднее суммарное время просмотра одной приставкой за период, мин
+      mutate(duration_per_stb=round(channel_duration/unique_stb, 0))
+      # %>% mutate_at(vars(mean_duration, ratio_per_stb, watch_ratio, duration_per_stb), funs(round), digits=1)
+
+    # browser()
+    # dbDisconnect(con)
+    as_tibble(df)
+  })  
+
+  cur_df <- reactive({
+    req(raw_df()) %>%
+      mutate_at(vars(channelId), as.character) %>%
+      left_join(progs_df, by=c("channelId")) %>%
+      # санация
+      mutate(channelName=if_else(is.na(channelName), 
+                                 str_c("_", channelId, "_"), 
+                                 channelName)) %>%
+      select(channelName, channelId, everything())
+  })
+  
+  msg <- reactiveVal("")
+
+  # таблица с выборкой по каналам ----------------------------
+  output$stat_table <- DT::renderDataTable({
+    df <- req(cur_df()) %>%
+      select(-channelId)
+    
+    colnames_df <- getRusColnames(df)
+    # https://stackoverflow.com/questions/39970097/tooltip-or-popover-in-shiny-datatables-for-row-names
+    colheader <- htmltools::withTags(
+      table(class = 'display',
+            thead(
+              tr(colnames_df %>%
+                   {purrr::map2(.$col_label, .$col_runame_screen, ~th(title=.x, .y))})
+              )))
+    
+    # browser()
+    # https://rstudio.github.io/DT/functions.html
+    DT::datatable(df,
+                  rownames=FALSE,
+                  filter='bottom',
+                  # только после жесткой фиксации колонок
+                  container=colheader,
+                  options=list(dom='fltip', pageLength=7, lengthMenu=c(5, 7, 10, 15),
+                               order=list(list(3, 'desc')))) %>%
+      DT::formatPercentage("watch_ratio", 2) %>%
+      DT::formatPercentage("stb_ratio", 2)
+    })
+  
+  # график Топ10 каналов по суммарному времени просмотра -------------
+  output$top10_duration_plot <-renderPlot({
+    shiny::validate(
+      need(!is.null(cur_df()), "NULL value can't be renederd"),
+      need(nrow(cur_df())>0, "0 rows -- nothing to draw") 
+    )
+    plotTop10Duration(cur_df(), publish_set=font_sizes[["screen"]])
+  })
+  
+  # график Топ10 каналов по количеству уникальных приставок --------------
+  output$top10_stb_plot <-renderPlot({
+    shiny::validate(
+      need(!is.null(cur_df()), "NULL value can't be renederd"),
+      need(nrow(cur_df())>0, "0 rows -- nothing to draw") 
+    )
+    plotTop10STB(cur_df(), publish_set=font_sizes[["screen"]])
+  })  
+
+  # динамическое управление диапазоном дат ---------
+  observeEvent(input$history_depth, {
+    # $history_depth получаем как строку
+    date <- Sys.Date()-as.numeric(input$history_depth)
+    flog.info(paste0("Start date changed to  ", date))
+    # updateDateRangeInput(session, "in_date_range", start=date)
+    }
+  )
+
+  # фиксим даты на демо диапазон ---------  
+  observeEvent(input$set_test_dates_btn, {
+    updateDateRangeInput(session, "in_date_range", start="2017-05-28", end="2017-05-30")
+    }
+  )
+  
+  # управляем визуализацией кнопок выгрузки ----- 
+  observe({
+    # browser()
+    if(!is.null(cur_df()) & nrow(cur_df())>0) {
+      shinyjs::enable("csv_download_btn")
+      shinyjs::enable("word_download_btn")
+    } else {
+      shinyjs::disable("csv_download_btn")
+      shinyjs::disable("word_download_btn")
+    }
+  })  
+  
+  # служебный вывод ---------------------  
+  output$info_text <- renderText({
+    msg()
+  })
+
+
+  # динамический выбор региона ---------
+  output$choose_region <- renderUI({
+    
+    data <- as.list(cities_df$translit)
+    names(data) <- cities_df$russian
+    
+    # browser()
+    
+    # создадим элемент
+    selectInput("region_filter", 
+                paste0("Регион (", length(data), ")"),
+                multiple=TRUE,
+                choices=data, width = "100%")
+  })
+
+  # обработчики кнопок выгрузки файлов --------------------------------------------------
+  # выгрузка таблицы в CSV -----------------------  
+  output$csv_download_btn <- downloadHandler(
+    filename = function() {
+      paste0("channel_rating_data-", Sys.Date(), ".csv", sep="")
+    },
+    content = function(file) {
+      cur_df() %>%
+        # сделаем вывод в формате, принимаемым Excel
+        write.table(file, na="NA", append=FALSE, col.names=TRUE, 
+                    row.names=FALSE, sep=";", fileEncoding="windows-1251")
+    }
+  )
+  
+  # выгрузка таблицы в Word -----------------------
+  output$word_download_btn <- downloadHandler(
+    filename = function() {
+      name <- paste0("channel_rating_report-", Sys.Date(), ".docx", sep="")
+      flog.info(paste0("Word report: '", name, "'"))
+      name
+    },
+    content = function(file) {
+      # browser();
+      doc <- cur_df() %>% # select(-total_unique_stb) %>% # пока убираем, чтобы была консистентная подстановка
+        gen_word_report(publish_set=font_sizes[["word_A4"]])
+      print(doc, target=file)  
+    }
+  )  
+  
+}
+
+shinyApp(ui = ui, server = server)
---
+++ 76 MTS_TV/00_general/funcs.R
@@ -0,0 +1,202 @@
+hgroup.enum <- function(date, hour_bin=NULL, min_bin=5){
+  # привязываем все измерения, которые попали в промежуток [0, t] к точке измерения.
+  # точки измерения могут быть кратны 1, 2, 3, 4, 6, 12 часам, определяется hour.bin
+  # отсчет измерений идет с 0:00
+  # поправка для лаборатории. для группировки меньше часа допускается указывать числа меньше 1
+  # 0.5 -- раз в полчаса.0.25 -- раз в 15 минут
+  # если hour.bin=NULL, то идет привязка к интервалам min.bin, заданном в минутах
+  # необходим пакет lubridate
+  
+  tick_time <- date
+  
+  if (is.null(hour_bin)){
+    # привязываем к минутным интервалам
+    n <- floor(minute(tick_time)/min_bin)
+    dt <- floor_date(tick_time, unit="hour") + minutes(n * min_bin)
+    
+  }else{
+    # привязываем к часовым интервалам
+    if (hour_bin < 1 & !(hour_bin %in% c(0.25, 0.5))) hour_bin=1
+    n <- floor((hour(tick_time)*60 + minute(tick_time))/ (hour_bin*60))
+    dt <- floor_date(tick_time, unit="day") + minutes(n * hour_bin*60)
+  }
+  
+  dt
+}
+
+# конструирование ограничений запроса по данным фильтров
+buildReqLimits <- function(begin, end, regions, segment) {
+  # базисная SQL конструкция для ограничения дат ----
+  # limit_dates <- paste0(" toDate(begin) >= toDate('", begin, "') AND toDate(end) <= toDate('", end, "') ")
+  limit_dates <- paste0(" date >= '", begin, "' AND date <= '", end, "' ")
+  
+  # добавочная SQL конструкция для ограничения регионов -----
+  limit_regions <- ifelse(is.null(regions), " ",
+                          stri_join(" AND region IN (", 
+                                    stri_join(regions %>% map_chr(~stri_join("'", .x, "'", sep="")),
+                                              sep = " ", collapse=","),
+                                    ") ", sep = "", collapse=""))
+  
+  # добавочная SQL конструкция для ограничения сегментов -----
+  limit_segments <- ifelse(segment=="all", " ", 
+                           stri_join(" AND segment IN (", 
+                                     stri_join(segment %>% map_chr(~stri_join("'", .x, "'", sep="")),
+                                               sep = " ", collapse=","),
+                                     ") ", sep = "", collapse=""))
+  
+  paste0(limit_dates, limit_regions, limit_segments)
+}
+
+# построение запроса для отчета 'Рейтинг по каналам' ----------------
+buildReq <- function(begin, end, regions, segment="all"){
+  # begin, end -- даты; 
+  # regs -- вектор регионов, если NULL -- то все регионы (в т.ч. на этапе инициализации);
+  # segment -- регион (строка), если "all" -- то все сегменты;
+  # browser()
+  
+  limits <- buildReqLimits(begin, end, regions, segment)
+
+  paste(
+    "SELECT ",
+    # 1. Название канала
+    "channelId, ",
+    # 2. Кол-во уникальных приставок по каналу
+    "uniq(serial) AS unique_stb, ",
+    # Кол-во уникальных приставок по всем каналам выбранных регионов
+    "( SELECT uniq(serial) ",
+    "  FROM genstates ",
+    "  WHERE ", limits, 
+    "  AND duration>5*60 AND duration <2*60*60 ", # указали жестко длительность, в секундах
+    ") AS total_unique_stb, ",  
+    # 4. Суммарное время просмотра всеми приставками, мин
+    "sum(duration)/60 AS channel_duration, ",
+    # 8. Кол-во событий просмотра
+    "count() AS watch_events ",
+    "FROM genstates ",
+    # "SAMPLE 0.1 ",
+    "WHERE ", limits,
+    "AND duration>5*60 AND duration <2*60*60 ", # указали жестко длительность, в секундах
+    "GROUP BY channelId", sep="")
+}
+
+# построение гистограммы ТОП 10 по времени просмотра для отчета 'Рейтинг по каналам' ----------------
+plotTop10Duration <- function(df, publish_set, ntop=10){
+  
+  flog.info(paste0("publish_set is ", capture.output(str(publish_set))))
+  # выберем наиболее программы c позиции эфирного времени
+  reg_df <- df %>%
+    top_n(ntop, channel_duration) %>%
+# может возникнуть ситуация, когда все значения top_n одинаковы. тогда надо брать выборку
+    filter(row_number()<=ntop) %>%
+    arrange(desc(channel_duration)) %>%
+    mutate(label=format(channel_duration, big.mark=" "))
+
+  gp <- ggplot(reg_df, aes(fct_reorder(as.factor(channelName), channel_duration, .desc=FALSE), channel_duration)) +
+    geom_bar(fill=brewer.pal(n=9, name="Greens")[4], alpha=0.5, stat="identity") +
+    # geom_text(aes(label=label), hjust=+1.1, colour="blue") + # для вертикальных
+    geom_label(aes(label=label), fill="white", colour="black", fontface="bold", hjust=+1.1) +
+    # geom_text_repel(aes(label=label), fontface = 'bold', color = 'blue', nudge_y=0) +
+    # scale_x_discrete("Передача", breaks=df2$order, labels=df2$channelName) +
+    scale_y_log10() +
+    theme_ipsum_rc(base_size=publish_set[["base_size"]],
+                   subtitle_size=publish_set[["subtitle_size"]],
+                   axis_title_size=publish_set[["axis_title_size"]]) +  
+    theme(axis.text.x = element_text(angle=90)) +
+    ylab("Суммарное количество минут") +
+    xlab("Канал") +
+    ggtitle("Топ 10 каналов", subtitle="По времени телесмотрения") +
+    coord_flip() 
+  
+  gp
+}
+
+# построение гистограммы ТОП 10 по количеству уникальных приставок для отчета 'Рейтинг по каналам' ----------------
+plotTop10STB <- function(df, publish_set, ntop=10){
+  
+  flog.info(paste0("publish_set is ", capture.output(str(publish_set))))
+  # выберем наиболее программы c позиции эфирного времени
+  reg_df <- df %>%
+    top_n(ntop, unique_stb) %>%
+    filter(row_number()<=ntop) %>% # на случай одинаковых значений
+    arrange(desc(unique_stb)) %>%
+    mutate(label=format(unique_stb, big.mark=" "))    
+  
+  gp <- ggplot(reg_df, aes(fct_reorder(as.factor(channelName), unique_stb, .desc=FALSE), unique_stb)) +
+    geom_bar(fill=brewer.pal(n=9, name="Blues")[4], alpha=0.5, stat="identity") +
+    # geom_text(aes(label=label), hjust=+1.1, colour="blue") + # для вертикальных
+    geom_label(aes(label=label), fill="white", colour="black", fontface="bold", hjust=+1.1) +
+    # geom_text_repel(aes(label=label), fontface = 'bold', color = 'blue', nudge_y=0) +
+    # scale_x_discrete("Передача", breaks=df2$order, labels=df2$channelName) +
+    scale_y_log10() +
+    theme_ipsum_rc(base_size=publish_set[["base_size"]], 
+                   subtitle_size=publish_set[["subtitle_size"]],
+                   axis_title_size=publish_set[["axis_title_size"]]) +  
+    theme(axis.text.x = element_text(angle=90)) +
+    ylab("Количество уникальных приставок") +
+    xlab("Канал") +
+    ggtitle("Топ 10 каналов", subtitle="По количеству уникальных приставок") +
+    coord_flip() 
+  
+  gp
+}
+
+# Генерация word файла для выгрузки средcтвами officer -------------
+gen_word_report <- function(df, template_fname, publish_set=NULL){
+  if(is.na(publish_set)){
+    flog.error("publish_set is NULL")
+    return(NULL)
+  }
+  # считаем данные для вставки -----------------------------------
+  n_out <- ifelse(nrow(df)<80, nrow(df), 80)
+  out_df <- df %>% 
+    filter(row_number() < n_out) %>%
+    select(-total_unique_stb)
+
+  flog.info(paste0("Word report generation under ", Sys.info()["sysname"]))
+  if (Sys.info()["sysname"] == "Linux") {
+    names_df <- getRusColnames(out_df)
+    names(out_df) <- names_df$col_runame_office
+  }
+  
+  # создаем файл ------------------------------------------
+  doc <- read_docx() %>% # read_docx(path="./TV_report_template.docx") %>%
+    body_add_par(value=paste0("Первые ", n_out, " строк данных"), style="heading 1") %>%
+    body_add_table(value=out_df, style="table_template") %>% 
+    body_add_par(value="ТОП 10 по времени просмотра", style="heading 2") %>%
+    body_add_gg(value=plotTop10Duration(df, publish_set=publish_set), style = "centered") %>%
+    body_add_par(value="ТОП 10 по количеству уникальных приставок", style="heading 2") %>%
+    body_add_gg(value=plotTop10STB(df, publish_set=publish_set), style="centered")
+  
+  doc
+  
+  }
+
+# Локализация названий колонок в датасете --------------
+getRusColnames <- function(df) {
+  colnames_df <- tribble(
+    ~col_name, ~col_runame_screen, ~col_runame_office, ~col_label, 
+    "region", "регион", "регион","Название региона, указанного в настройках STB",
+    "unique_stb", "кол-во уник. STB", "кол-во уник. STB", "Кол-во уникальных STB, с которых был зафиксирован хотя бы один факт телесмотрения канала",
+    "total_unique_stb", "всего уник. STB", "всего уник. STB", "Общее количество уникальных STB, с которых выполнялось телесмотрение хотя бы одного канала",
+    "total_duration", "суммарное время, мин",	"суммарное время, мин",	"Суммарное время просмотра канала всеми STB",
+    "watch_events", "кол-во просмотров", "кол-во просмотров", "Количество событий телесмотрения канала со всех приставок",
+    "stb_ratio", "% уник. STB", "% уник. STB", "Отношение STB, смотревших данный канал, к общему количеству уникальных STB",
+    "segment", "сегмент", "сегмент", "подсказка (segment)",
+    "channelId", "канал (ID)", "канал  (ID)", "подсказка (channelId)",
+    "channelName", "канал", "канал", "Название телеканала",
+    "channel_duration", "суммарное время, мин", "суммарное время, мин", "подсказка (channel_duration)",
+    "mean_duration", "ср. время просмотра, мин", "ср. время просмотра, мин", "Среднее время отдельного просмотра канала",
+    "watch_ratio", "% врем. просмотра", "% врем. просмотра", "Отношение времени просмотра канала к общему времени телесмотрения",
+    "duration_per_stb", "ср. время просм. 1 STB за период, мин", "ср. время просм. 1 STB за период, мин", "Среднее время суммарного просмотра канала одной приставкой за выбранный период",
+    "date", "дата", "дата", "подсказка (date)",
+    "timestamp", "время", "время", "подсказка (timestamp)",
+    "timegroup", "группа", "группа", "подсказка (timegroup)"
+  )
+  
+  tibble(name=names(df)) %>%
+    left_join(colnames_df, by=c("name"="col_name")) %>%
+    # санация
+    mutate(col_runame_screen=if_else(is.na(col_runame_screen), name, col_runame_screen)) %>%
+    mutate(col_runame_office=if_else(is.na(col_runame_office), name, col_runame_office)) %>%
+    mutate(col_label=if_else(is.na(col_label), name, col_label))
+}
