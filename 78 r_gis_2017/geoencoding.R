adc43ff556d82c3149df76e350f52f0452f1a598
iMissile <ishutov@gmail.com>
2017-08-21 20:01:30 +0300

    Геокодирование на google api вроде работает

--- 78 r_gis_2017/geoencoding.R
+++ 78 r_gis_2017/geoencoding.R
@@ -1,10 +1,20 @@
-library(ggmap)
+# library(ggmap)
 library(tmap)
 library(tmaptools) #geocode
 
 # map <- get_map(location = 'uk', zoom = 8, source = "osm") # ошибка!!!
 
 data(Europe)
-qtm(Europe)
+# qtm(Europe)
 
+# ----
+# eiffelTower <- geocode_OSM("Eiffel Tower", as.SPDF=TRUE)
+city_geocode <- geocode_OSM("Москва,+Тверская+улица,+дом+7", as.SPDF=TRUE)
+sp::coordinates(city_geocode) <- ~lon+lat
+
+data(World)
+tm_shape(World, bbox="Russia")
+
+tm_shape(city_geocode) +
+  tm_dots(col = "red")
 
