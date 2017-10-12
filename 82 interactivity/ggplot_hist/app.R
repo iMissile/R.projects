25c54f46b22a23ea847697eb0996dd1802d2b656
iMissile <ishutov@gmail.com>
2017-10-12 18:45:26 +0300

    Сделал facet stacked диаграмму с тултипами

--- 82 interactivity/ggplot_hist/app.R
+++ 82 interactivity/ggplot_hist/app.R
@@ -1,14 +1,10 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# https://shiny.rstudio.com/articles/plot-interaction-advanced.html
+# https://stackoverflow.com/questions/41654801/r-shiny-plot-click-with-geom-bar-and-facets
+
 library(tidyverse)
 library(lubridate)
 library(shiny)
+library(ggiraph)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -19,12 +15,16 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-        verbatimTextOutput("message_log")
+        verbatimTextOutput("info1"),
+        verbatimTextOutput("info2")
       ),
       
-      # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+        fluidRow(
+          # в ggigraphOutput height не работает, надо задавать размер svg в ggiraph
+          column(6, ggiraphOutput("interactivePlot", height="500px")),
+          column(6, plotOutput("staticPlot", click="plot_click"))
+        )
       )
    )
 )
@@ -32,18 +32,47 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-  demo_df <- tibble(category=ymd("2017-10-12", "2017-10-11", "2017-10-10", "2017-10-9"),
+  tmp_df <- tibble(category=ymd("2017-10-12", "2017-10-11", "2017-10-9", "2017-10-8"),
                     value=c(4.2, 7.7, 3.9, 6.2),
-                    type=c("BAD", "OK", "OK", "BAD"))
+                    type=c("ПЛОХО", "ХОРОШО", "ХОРОШО", "ПЛОХО")) %>%
+    mutate(layer="L_1")
   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
+  demo_df <- bind_rows(tmp_df, tmp_df %>% mutate(value=round(value+runif(n(), -1, 1), 1), layer="L_2")) %>%
+    mutate(id=as.character(row_number()), tooltip=as.character(value))
+  
+   output$staticPlot <- renderPlot({
      gp <- ggplot(demo_df, aes(x=category, y=value)) +
-       geom_bar(aes(fill=type), stat="identity") +
-       scale_fill_brewer(palette="Dark2")
+       geom_bar(aes(fill=type), stat="identity", position="stack") +
+       scale_fill_brewer(palette="Dark2") +
+       facet_wrap(~type)
      
      gp
    })
+
+   output$interactivePlot <- renderggiraph({
+     gp <- ggplot(demo_df, aes(x=category, y=value, tooltip=value, data_id=id)) +
+       geom_bar_interactive(aes(fill=type, alpha=layer), stat="identity") +
+       scale_fill_brewer(palette="Dark2") +
+       scale_alpha_manual(values=c("L_1"=0.5, "L_2"=1)) +
+       facet_wrap(~type)
+     
+     # по умолчания ggigraph делает svg viewbox размером 6in x 6in!!!
+     ggiraph(code=print(gp), height_svg=300/72)
+     # ggiraph(code=print(gp))
+   })
+
+   output$info1 <- renderText({
+     # browser()
+     t <- input$plot_click
+     paste0("x=", input$plot_click$x, "\ny=", input$plot_click$y, "\nfacet=", input$plot_click$panelvar1)
+   })   
+
+   output$info2 <- renderText({
+     # browser()
+     t <- input$plot_click
+     paste0("x=", input$plot_click$x, "\ny=", input$plot_click$y, "\nfacet=", input$plot_click$panelvar1)
+   })   
+   
 }
 
 # Run the application 
