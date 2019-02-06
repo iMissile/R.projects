4045707a84cbd107e3e65b8c4fc573678b9b6fd6
iMissile <ishutov@gmail.com>
2019-02-06 10:31:03 +0300

    [wip] небольшие модификации при обучении для X5

--- 71 report_samples/compile_xDR_all.cmd
+++ 71 report_samples/compile_xDR_all.cmd
@@ -2,4 +2,4 @@ set PATH=c:\Program Files\RStudio\bin\pandoc;%PATH%
 
 PATH
 
-R -e "rmarkdown::render('xDR_analysis.Rmd', output_format='all', encoding = 'UTF-8')"
+C:\Program Files\R\R-3.5.2\bin\R.exe -e "rmarkdown::render('xDR_analysis.Rmd', output_format='all', encoding = 'UTF-8')"
---
+++ 77 artem/scorecard_demo/run_shiny_app.cmd
@@ -0,0 +1,3 @@
+rem https://shiny.rstudio.com/articles/running.html
+rem "C:\Program Files\R\R-3.4.4\bin\x64\Rscript.exe"  -e "Sys.setenv(LANG='C'); shiny::runApp('app.R')"
+"C:\Program Files\R\R-3.5.2\bin\x64\Rscript.exe"  -e "Sys.setenv(LANG='C'); shiny::runApp('app.R')"
