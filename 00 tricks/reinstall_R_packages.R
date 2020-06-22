9112f88d0401db1ae709b3850a3b155a739955b9
iMissile <ishutov@gmail.com>
2020-06-22 23:08:45 +0300

    [wip] методика ручной переустановки пакетов при смене major версии R

---
+++ 00 tricks/reinstall_R_packages.R
@@ -0,0 +1,70 @@
+# https://ibecav.github.io/update_libraries/
+require(tidyverse)
+
+# Helpful background
+allmypackages <- as.data.frame(installed.packages())
+
+allmypackages <- allmypackages %>%
+  filter(Priority != "base" | is.na(Priority)) %>%
+  select(-c(Enhances:MD5sum, LinkingTo:Suggests)) %>%
+  droplevels()
+
+str(allmypackages)
+
+# A function to do the hard work
+
+package_source <- function(pkg){
+  x <- as.character(packageDescription(pkg)$Repository)
+  if (length(x)==0) {
+    y <- as.character(packageDescription(pkg)$GithubRepo)
+    z <- as.character(packageDescription(pkg)$GithubUsername)
+    if (length(y)==0) {
+      return("Other")
+    } else {
+      return(str_c("GitHub repo = ", z, "/", y))
+    }
+  } else {
+    return(x)
+  }
+}
+
+# show the first 60 as an example
+head(sapply(allmypackages$Package, package_source), 60)
+
+# What’s in your libraries?
+allmypackages$whereat <- sapply(allmypackages$Package, package_source)
+str(allmypackages)
+
+table(allmypackages$whereat)
+
+allmypackages %>% 
+  filter(whereat == "Other") %>%
+  select(Package, Version)
+
+write.csv(allmypackages, "mypackagelistJune2020.csv")
+
+# ======================================
+# Go ahead and install R 3.6.0
+# post upgrade with output surpessed
+install.packages("tidyverse")
+library(tidyverse)
+
+oldpackages <- read.csv("mypackagelistJune2020.csv")
+allmypackages <- as.data.frame(installed.packages())
+allmypackages <- allmypackages %>%
+  filter(Priority != "base" | is.na(Priority)) %>%
+  select(-c(Enhances:MD5sum, LinkingTo:Suggests))
+thediff <- anti_join(oldpackages,allmypackages, by = "Package")
+
+# Just do it!
+
+thediff %>%
+  filter(whereat == "CRAN") %>%
+  pull(Package) %>%
+  as.character %>%
+  install.packages
+
+# Что осталось за бортом?
+thediff %>%
+  filter(whereat != "CRAN") %>%
+  select(Package, Version, NeedsCompilation, whereat)
