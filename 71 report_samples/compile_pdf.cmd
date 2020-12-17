380db6d80242a1357979dc694dce172223c5ff01
iMissile <ishutov@gmail.com>
2020-12-17 12:54:49 +0300

    правки

--- 71 report_samples/compile_pdf.cmd
+++ 71 report_samples/compile_pdf.cmd
@@ -2,4 +2,4 @@ set PATH=c:\Program Files\RStudio\bin\pandoc;%PATH%
 
 PATH
 
-"C:\Program Files\R\R-3.5.2\bin\R.exe" -e "rmarkdown::render('ru-tex-pdf-examples.Rmd', encoding = 'UTF-8')"
+"C:\Program Files\R\R-3.6.1\bin\R.exe" -e "rmarkdown::render('ru-tex-pdf-examples.Rmd', encoding = 'UTF-8')"
